7d90561f24d872dd563448e15ec654544d1b4d25
James Shore <jshore@jamesshore.com>
2012-07-10 15:46:09 -0700

    Made Lint check all files in tree (except node modules)

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,20 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
+
 task("default", ["lint"]);
 
 desc("Lint everything");
 task("lint", [], function() {
 	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("jakefile.js", {}, {});
+
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+
+	var options = {
+		node: true
+	};
+
+	lint.validateFileList(files.toArray(), options, {});
 });
