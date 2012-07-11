2ef167dbdece2c0b60e5325f27180956ebdfb4a6
James Shore <jshore@jamesshore.com>
2012-07-11 11:10:00 -0700

    Set up strict Lint options

--- jakefile.js
+++ jakefile.js
@@ -1,20 +1,37 @@
 /*global desc, task, jake, fail, complete */
+(function() {
+	"use strict";
 
-"use strict";
+	task("default", ["lint"]);
 
-task("default", ["lint"]);
+	desc("Lint everything");
+	task("lint", [], function() {
+		var lint = require("./build/lint/lint_runner.js");
 
-desc("Lint everything");
-task("lint", [], function() {
-	var lint = require("./build/lint/lint_runner.js");
+		var files = new jake.FileList();
+		files.include("**/*.js");
+		files.exclude("node_modules");
+		var options = nodeLintOptions();
+		lint.validateFileList(files.toArray(), options, {});
+	});
 
-	var files = new jake.FileList();
-	files.include("**/*.js");
-	files.exclude("node_modules");
-
-	var options = {
-		node: true
-	};
-
-	lint.validateFileList(files.toArray(), options, {});
-});
+	function nodeLintOptions() {
+		return {
+			bitwise:true,
+			curly:false,
+			eqeqeq:true,
+			forin:true,
+			immed:true,
+			latedef:true,
+			newcap:true,
+			noarg:true,
+			noempty:true,
+			nonew:true,
+			regexp:true,
+			undef:true,
+			strict:true,
+			trailing:true,
+			node:true
+		};
+	}
+}());
