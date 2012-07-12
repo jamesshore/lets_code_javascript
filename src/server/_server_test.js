b577c2d994f4c2d2d0f06d103c7cbdbc4db92657
James Shore <jshore@jamesshore.com>
2012-07-12 12:46:26 -0700

    Switched tests back to nodeunit assertions so assertion count works

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,9 +2,8 @@
 "use strict";
 
 var server = require("./server.js");
-var assert = require("assert");
 
 exports.testNothing = function(test) {
-	assert.equal(3, server.number(), "number");
+	test.equals(3, server.number(), "number");
 	test.done();
 };
