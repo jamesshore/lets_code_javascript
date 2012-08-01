84521d7e257ccc8630567a9c896bdc784c51ea4e
James Shore <jshore@jamesshore.com>
2012-08-01 13:44:16 -0700

    Finished basic 'hello world' server--still needs cleanup

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -13,9 +13,21 @@ exports.tearDown = function(done) {
 //TODO: handle case where stop() is called before start()
 //TODO: test-drive stop() callback
 
-exports.testServerRespondsToGetRequests = function(test) {
-	server.start();
-	http.get("http://localhost:8080", function(response) {
-		test.done();
+exports.test_serverReturnsHelloWorld = function(test) {
+	server.start(); //TODO: remove duplication?
+	var request = http.get("http://localhost:8080");
+	request.on("response", function(response) {
+		var receivedData = false;
+		response.setEncoding("utf8");
+
+		test.equals(200, response.statusCode, "status code");
+		response.on("data", function(chunk) {
+			receivedData = true;
+			test.equals("Hello World", chunk, "response text");
+		});
+		response.on("end", function() {
+			test.ok(receivedData, "should have received response data");
+			test.done();
+		});
 	});
 };
--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ var server;
 exports.start = function() {
 	server = http.createServer();
 	server.on("request", function(request, response) {
-		response.end();
+		response.end("Hello World");
 	});
 	server.listen(8080);    //TODO: Remove duplication
 };
