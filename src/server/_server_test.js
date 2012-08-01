85adae84ae8f0d1ef4faf7d4b9d52e08375f03d1
James Shore <jshore@jamesshore.com>
2012-08-01 14:06:20 -0700

    Finished up 'hello world' server

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,20 +4,8 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.setUp = function(done) {
-	server.start(8080);
-	done();
-};
-
-exports.tearDown = function(done) {
-	server.stop(function() {
-		done();
-	});
-};
-
-//TODO: handle case where stop() is called before start()
-
 exports.test_serverReturnsHelloWorld = function(test) {
+	server.start(8080);
 	var request = http.get("http://localhost:8080");
 	request.on("response", function(response) {
 		var receivedData = false;
@@ -30,20 +18,30 @@ exports.test_serverReturnsHelloWorld = function(test) {
 		});
 		response.on("end", function() {
 			test.ok(receivedData, "should have received response data");
-			test.done();
+			server.stop(function() {
+				test.done();
+			});
 		});
 	});
 };
 
+exports.test_serverRequiresPortNumber = function(test) {
+	test.throws(function() {
+		server.start();
+	});
+	test.done();
+};
+
 exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
+	server.start(8080);
 	server.stop(function() {
 		test.done();
 	});
-	server.start(); //TODO: this is kludgy
 };
 
-exports.test_stopCalledTwiceInARow = function(test) {
-	server.stop();
-	server.stop();
-	server.start();
+exports.test_stopCalledWhenServerIsntRunningThrowsException = function(test) {
+	test.throws(function() {
+		server.stop();
+	});
+	test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,8 @@ var http = require("http");
 var server;
 
 exports.start = function(portNumber) {
+	if(!portNumber) throw "port number is required";
+
 	server = http.createServer();
 	server.on("request", function(request, response) {
 		response.end("Hello World");
