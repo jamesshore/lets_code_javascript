62470460671ebbd282e55d02a4026f063d6a025c
James Shore <jshore@jamesshore.com>
2012-08-01 13:22:22 -0700

    Got our first client/server test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,27 +4,15 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.tearDown = function(test) {
-	console.log("tear down start");
+exports.tearDown = function(done) {
 	server.stop(function() {
-		console.log("stop callback");
-		//TODO: need test.done() equivalent here
+		done();
 	});
-	console.log("tear down end");
 };
 
-//TODO: handle case where stop() is called before start()
-//TODO: test-drive stop() callback
-
-exports.testHttpServer = function(test) {
-	console.log("test started");
+exports.testServerRespondsToGetRequests = function(test) {
 	server.start();
-	console.log("server started");
-
 	http.get("http://localhost:8080", function(response) {
-		console.log("response callback");
 		test.done();
-		console.log("test done");
 	});
-	console.log("get called");
 };
--- src/server/server.js
+++ src/server/server.js
@@ -5,14 +5,21 @@ var http = require("http");
 var server;
 
 exports.start = function() {
+	console.log("start called");
 	server = http.createServer();
+	console.log("server called");
 
 	server.on("request", function(request, response) {
+		console.log("request called");
+		response.end();
 	});
+	console.log("'on' called");
 
 	server.listen(8080);
+	console.log("listen called");
 };
 
 exports.stop = function(callback) {
+	console.log("stop called");
 	server.close(callback);
 };
