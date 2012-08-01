1400ed04867dacd3a269113b18720eec18993746
James Shore <jshore@jamesshore.com>
2012-08-01 13:55:10 -0700

    Added test to make sure server callback is called when stop() called

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,6 +4,11 @@
 var server = require("./server.js");
 var http = require("http");
 
+exports.setUp = function(done) {
+	server.start(8080);
+	done();
+};
+
 exports.tearDown = function(done) {
 	server.stop(function() {
 		done();
@@ -14,7 +19,6 @@ exports.tearDown = function(done) {
 //TODO: test-drive stop() callback
 
 exports.test_serverReturnsHelloWorld = function(test) {
-	server.start(8080);
 	var request = http.get("http://localhost:8080");
 	request.on("response", function(response) {
 		var receivedData = false;
@@ -30,4 +34,11 @@ exports.test_serverReturnsHelloWorld = function(test) {
 			test.done();
 		});
 	});
+};
+
+exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
+	server.stop(function() {
+		test.done();
+	});
+	server.start(); //TODO: this is kludgy
 };
