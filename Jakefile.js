2c84605af4e2f8634467900f2c1719556fd58b12
James Shore <jshore@jamesshore.com>
2012-08-28 16:02:55 -0700

    Added end-of-episode checklist to Jakefile (for use when recording)

--- Jakefile.js
+++ Jakefile.js
@@ -43,19 +43,6 @@
 		});
 	}, {async: true});
 
-	desc("Integrate");
-	task("integrate", ["default"], function() {
-		console.log("1. Make sure 'git status' is clean.");
-		console.log("2. Build on the integration box.");
-		console.log("   a. Walk over to integration box.");
-		console.log("   b. 'git pull'");
-		console.log("   c. 'jake strict=true'");
-		console.log("   d. If jake fails, stop! Try again after fixing the issue.");
-		console.log("3. 'git checkout integration'");
-		console.log("4. 'git merge master --no-ff --log'");
-		console.log("5. 'git checkout master'");
-	});
-
 //	desc("Ensure correct version of Node is present. Use 'strict=true' to require exact match");
 	task("nodeVersion", [], function() {
 		function failWithQualifier(qualifier) {
@@ -79,6 +66,27 @@
 			if (actual[0] === expected[0] && actual[1] === expected[1] && actual[2] < expected[2]) failWithQualifier("at least");
 		}
 
+	});
+
+	desc("Integration checklist");
+	task("integrate", ["default"], function() {
+		console.log("1. Make sure 'git status' is clean.");
+		console.log("2. Build on the integration box.");
+		console.log("   a. Walk over to integration box.");
+		console.log("   b. 'git pull'");
+		console.log("   c. 'jake strict=true'");
+		console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+		console.log("3. 'git checkout integration'");
+		console.log("4. 'git merge master --no-ff --log'");
+		console.log("5. 'git checkout master'");
+	});
+
+	desc("End-of-episode checklist");
+	task("episode", [], function() {
+		console.log("1. Save recording.");
+		console.log("2. Double-check sound and framing.");
+		console.log("3. Commit source code.");
+		console.log("4. Tag episode: 'git tag -a episodeXX -m \"End of episode XX\"'");
 	});
 
 	function parseNodeVersion(description, versionString) {
