c4ed57d8d69dd7fba9ca30e97a31ca46d41e767f
James Shore <jshore@jamesshore.com>
2012-08-28 13:24:34 -0700

    Smoke test runs process

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -3,3 +3,22 @@
 // launch the server in the same way it happens in production
 // get a page
 // confirm we got something
+
+(function() {
+	"use strict";
+
+	var child_process = require("child_process");
+
+	exports.test_for_smoke = function(test) {
+		var command = "node weewikipaint 8080";
+		child_process.exec(command, function(error, stdout, stderr) {
+			if (error !== null) {
+				console.log(stdout);
+				console.log(stderr);
+				throw error;
+			}
+			test.done();
+		});
+	};
+
+}());
