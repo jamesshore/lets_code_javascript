1eb1795215c9c585b026d0eaaadea070b0ae6a85
James Shore <jshore@jamesshore.com>
2012-08-28 13:11:01 -0700

    Jakefile now runs all test files

--- Jakefile.js
+++ Jakefile.js
@@ -22,18 +22,22 @@
 	task("lint", ["nodeVersion"], function() {
 		var lint = require("./build/lint/lint_runner.js");
 
-		var files = new jake.FileList();
-		files.include("**/*.js");
-		files.exclude("node_modules");
+		var javascriptFiles = new jake.FileList();
+		javascriptFiles.include("**/*.js");
+		javascriptFiles.exclude("node_modules");
 		var options = nodeLintOptions();
-		var passed = lint.validateFileList(files.toArray(), options, {});
+		var passed = lint.validateFileList(javascriptFiles.toArray(), options, {});
 		if (!passed) fail("Lint failed");
 	});
 
 	desc("Test everything");
 	task("test", ["nodeVersion", TEMP_TESTFILE_DIR], function() {
+		var testFiles = new jake.FileList();
+		testFiles.include("**/_*_test.js");
+		testFiles.exclude("node_modules");
+
 		var reporter = require("nodeunit").reporters["default"];
-		reporter.run(['src/server/_server_test.js'], null, function(failures) {
+		reporter.run(testFiles.toArray(), null, function(failures) {
 			if (failures) fail("Tests failed");
 			complete();
 		});
