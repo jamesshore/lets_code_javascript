fe8222f99caf465cd7716d745250cd3f707f8ac1
James Shore <jshore@jamesshore.com>
2012-08-28 11:53:16 -0700

    Pseudocode skeleton for smoke test

---
+++ src/_smoke_test.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+
+// launch the server in the same way it happens in production
+// get a page
+// confirm we got something
