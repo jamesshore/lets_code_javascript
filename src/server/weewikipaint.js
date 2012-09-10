8ad84b5a11b3657c8116b0b32af826600781c3ab
James Shore <jshore@jamesshore.com>
2012-09-10 12:18:54 -0700

    Factored out content directory

--- src/server/weewikipaint.js
+++ src/server/weewikipaint.js
@@ -2,10 +2,11 @@
 (function() {
 	"use strict";
 
+	var CONTENT_DIR = "src/server/content";
 
 	var server = require("./server.js");
 	var port = process.argv[2];
-	server.start("src/server/content/homepage.html", "src/server/content/404.html", port, function() {
+	server.start(CONTENT_DIR + "/homepage.html", CONTENT_DIR + "/404.html", port, function() {
 		console.log("Server started");
 	});
 }());
