b5963a156ee91c0960afd17c4f70c9fbe50d5636
James Shore <jshore@jamesshore.com>
2012-09-17 12:18:33 -0700

    Added release test

---
+++ src/server/_release_test.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+/*jshint regexp:false*/
+
+(function() {
+	"use strict";
+
+	var jake = require("jake");
+	var child_process = require("child_process");
+	var http = require("http");
+	var fs = require("fs");
+	var procfile = require("procfile");
+	var child;
+
+	exports.test_isOnWeb = function(test) {
+		httpGet("http://weewikipaint.herokuapp.com", function(response, receivedData) {
+			var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
+			test.ok(foundHomePage, "home page should have contained test marker");
+			test.done();
+		});
+	};
+
+	function httpGet(url, callback) {
+		var request = http.get(url);
+		request.on("response", function(response) {
+			var receivedData = "";
+			response.setEncoding("utf8");
+
+			response.on("data", function(chunk) {
+				receivedData += chunk;
+			});
+			response.on("end", function() {
+				callback(response, receivedData);
+			});
+		});
+	}
+
+}());
