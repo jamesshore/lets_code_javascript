2e10f65b8839121b73724ad519fd86ce7434b431
James Shore <jshore@jamesshore.com>
2012-09-28 10:33:22 -0700

    Upgraded Node and npm versions

--- Jakefile.js
+++ Jakefile.js
@@ -4,7 +4,7 @@
 (function() {
 	"use strict";
 
-	var NODE_VERSION = "v0.8.6";
+	var NODE_VERSION = "v0.8.11";
 	var GENERATED_DIR = "generated";
 	var TEMP_TESTFILE_DIR = GENERATED_DIR + "/test";
 
