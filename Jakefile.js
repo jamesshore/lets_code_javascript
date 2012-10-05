e712b5a4c6f570ab0db199355142a1396ddbc70e
James Shore <jshore@jamesshore.com>
2012-10-05 14:24:52 -0700

    Fixed outdated 'strict=true' documentation in integration checklist

--- Jakefile.js
+++ Jakefile.js
@@ -116,7 +116,7 @@
 		console.log("2. Build on the integration box.");
 		console.log("   a. Walk over to integration box.");
 		console.log("   b. 'git pull'");
-		console.log("   c. 'jake strict=true'");
+		console.log("   c. 'jake'");
 		console.log("   d. If jake fails, stop! Try again after fixing the issue.");
 		console.log("3. 'git checkout integration'");
 		console.log("4. 'git merge master --no-ff --log'");
