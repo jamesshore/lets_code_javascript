405109accb46bc09253e4aef276b3645a53f5cef
James Shore <jshore@jamesshore.com>
2012-10-19 11:31:30 -0700

    Pulled information out of Testacular DOM

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -9,8 +9,14 @@
 		it("should run", function () {
 			var div = document.createElement("div");
 			div.setAttribute("id", "tdjs");
+			div.setAttribute("foo", "bar");
 			document.body.appendChild(div);
 			dump("breakpoint here");
+
+
+			var extractedDiv = document.getElementById("tdjs");
+			expect(extractedDiv.getAttribute("foo")).to.equal("bar");
+
 		});
 
 	});
