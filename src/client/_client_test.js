bfa16f4530747c2d4a0757f7f4f62b3f0f3a6388
James Shore <jshore@jamesshore.com>
2012-10-19 11:12:30 -0700

    Starting work on testing DOM

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,14 +1,15 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect*/
+/*global describe, it, expect, dump*/
 
 (function () {
 	"use strict";
-//var assert = cha.assert;
 
 	describe("Nothing", function () {
 
 		it("should run", function () {
-			expect("foo").to.equal("foo");
+
+
+			dump(document.body);
 		});
 
 	});
