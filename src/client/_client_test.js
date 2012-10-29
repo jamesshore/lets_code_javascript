12121354d175de68bb6ab293a17948906adaf7d6
James Shore <jshore@jamesshore.com>
2012-10-29 11:11:21 -0700

    Got our first full client-side test working (doesn't do anything real yet)

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect, dump*/
+/*global describe, it, expect, dump, wwp*/
 
 (function () {
 	"use strict";
@@ -7,6 +7,8 @@
 	describe("Nothing", function () {
 
 		it("should run", function () {
+      wwp.createElement();
+
 			var extractedDiv = document.getElementById("tdjs");
 			expect(extractedDiv.getAttribute("foo")).to.equal("bar");
 		});
--- src/client/client.js
+++ src/client/client.js
@@ -1,16 +1,16 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global dump*/
+/*global dump, wwp:true*/
+
+wwp = {};
 
 (function() {
 	"use strict";
 
-	$(function() {
+  wwp.createElement = function() {
 		var div = document.createElement("div");
 		div.setAttribute("id", "tdjs");
 		div.setAttribute("foo", "bar");
 		document.body.appendChild(div);
-
-		dump("Window loaded");
-	});
+	};
 
 }());
