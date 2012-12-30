e23cb180b4b0a901f845a76646d33adef4dba2bd
James Shore <jshore@jamesshore.com>
2012-12-30 14:40:37 -0800

    Made our drag state handling a bit more compact

--- src/client/client.js
+++ src/client/client.js
@@ -10,54 +10,24 @@ wwp = {};
 
 		wwp.initializeDrawingArea = function(drawingAreaElement) {
 			var start = null;
-			var isDragging = false;
 
 			paper = new Raphael(drawingAreaElement);
 
 			$(document).mousedown(function(event) {
-				isDragging = true;
 				start = relativeOffset(drawingArea, event.pageX, event.pageY);
 			});
 			$(document).mouseup(function(event) {
-				isDragging = false;
+				start = null;
 			});
 
 			var drawingArea = $(drawingAreaElement);
 			drawingArea.mousemove(function(event) {
+				if (start === null) return;
+
 				var end = relativeOffset(drawingArea, event.pageX, event.pageY);
-				if (start !== null && isDragging) wwp.drawLine(start.x, start.y, end.x, end.y);
+				wwp.drawLine(start.x, start.y, end.x, end.y);
 				start = end;
 			});
-
-//			var prevX = null;
-//			var prevY = null;
-//
-//			var jqArea = $(drawingAreaElement);
-//
-//			var isDragging = false;
-//
-//
-//			// TODO in test: if mouse clicked when outside of element, or let go outside of element,
-//			// then the 'isDragging' state could get stuck
-//			$(document).mousedown(function(event) {
-//				isDragging = true;
-//			});
-//			$(document).mouseup(function(event) {
-//				isDragging = false;
-//			});
-//
-//			jqArea.mousemove(function(event) {
-//				// TODO in test: account for padding, border, margin (manual tests too)
-//				var divPageX = jqArea.offset().left;
-//				var divPageY = jqArea.offset().top;
-//
-//				var relativeX = event.pageX - divPageX;
-//				var relativeY = event.pageY - divPageY;
-//
-//				if (prevX !== null && isDragging) wwp.drawLine(prevX, prevY, relativeX, relativeY);
-//				prevX = relativeX;
-//				prevY = relativeY;
-//			});
 			return paper;
 		};
 
