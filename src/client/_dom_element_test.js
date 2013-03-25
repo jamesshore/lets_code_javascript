1af11dd1f9603a3a40120ab1eb8939134a2e2858
James Shore <jshore@jamesshore.com>
2013-03-25 13:12:49 -0700

    Tested DomElement's mouse event methods

--- src/client/_dom_element_test.js
+++ src/client/_dom_element_test.js
@@ -1,13 +1,41 @@
 // Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect */
+/*global describe, it, expect, beforeEach, wwp, $ */
 
 (function() {
 	"use strict";
 
-	describe("nothing", function() {
 
-		it("should fail", function() {
+	describe("DomElement", function() {
+		var domElement;
+
+		beforeEach(function() {
+			domElement = new wwp.DomElement($("<div></div>"));
 		});
+
+		it("handles mouse events", function() {
+			testEvent(domElement.onSelectStart_ie8Only, domElement.doSelectStart);
+			testEvent(domElement.onMouseDown, domElement.doMouseDown);
+			testEvent(domElement.onMouseMove, domElement.doMouseMove);
+			testEvent(domElement.onMouseLeave, domElement.doMouseLeave);
+			testEvent(domElement.onMouseUp, domElement.doMouseUp);
+		});
+
+		it("handles touch events", function() {
+//				testEvent(domElement.onSingleTouchStart, domElement.doSingleTouchStart);
+//				testEvent(domElement.onSingleTouchMove, domElement.doSingleTouchMove);
+//				testEvent(domElement.onSingleTouchEnd, domElement.doSingleTouchEnd);
+//				testEvent(domElement.onSingleTouchCancel, domElement.doSingleTouchEnd);
+		});
+
+		function testEvent(eventSender, eventHandler) {
+			var eventOffset = null;
+			eventSender.call(domElement, function(offset) {
+				eventOffset = offset;
+			});
+			eventHandler.call(domElement, 42, 13);
+			expect(eventOffset).to.eql({ x: 42, y: 13});
+		}
+
 
 	});
 
