37d89e906f3b3aa2fce8397018a8d3c9750be2b2
James Shore <jshore@jamesshore.com>
2013-04-10 15:43:55 -0700

    Finished PhantomJS spike

---
+++ spikes/phantomjs/hello.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+(function() {
+	"use strict";
+
+	var page = require("webpage").create();
+
+	console.log("Hello world");
+
+	page.onConsoleMessage = function(message) {
+		console.log("CONSOLE: " + message);
+	};
+
+	page.open("http://localhost:5000", function(success) {
+		console.log("Success: " + success);
+
+		page.evaluate(inBrowser);
+
+		page.render("wwp.png");
+		phantom.exit();
+	});
+
+	function inBrowser() {
+		console.log("Hi");
+		console.log("defined? " + isDefined(wwp.HtmlElement));
+
+		var drawingArea = new wwp.HtmlElement($("#drawingArea"));
+		drawingArea.doMouseDown(10, 20);
+		drawingArea.doMouseMove(50, 60);
+		drawingArea.doMouseUp(50, 60);
+
+		function isDefined(obj) {
+			return typeof(obj) !== "undefined";
+		}
+	}
+
+}());
