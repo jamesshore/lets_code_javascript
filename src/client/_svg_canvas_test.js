8c0f7c14c6bb2ae0181228f9425cb9cf35f4c81d
James Shore <jshore@jamesshore.com>
2013-04-22 14:53:29 -0700

    Added tests to SvgCanvas

---
+++ src/client/_svg_canvas_test.js
@@ -0,0 +1,46 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+/*global HtmlElement, $ */
+
+(function() {
+	"use strict";
+
+	describe("SvgCanvas", function() {
+
+		var div;
+		var svgCanvas;
+
+		beforeEach(function() {
+			div = wwp.HtmlElement.fromHtml("<div></div>");
+			var documentBody = new wwp.HtmlElement($(document.body));
+			documentBody.append(div);
+			svgCanvas = new wwp.SvgCanvas(div);
+		});
+
+		afterEach(function() {
+			div.remove();
+			wwp.drawingAreaHasBeenRemovedFromDom();
+		});
+
+		it("returns zero line segments", function() {
+			expect(svgCanvas.lineSegments()).to.eql([]);
+		});
+
+		it("draws and returns one line segment", function() {
+			svgCanvas.drawLine(1, 2, 5, 10);
+			expect(svgCanvas.lineSegments()).to.eql([[1, 2, 5, 10]]);
+		});
+
+		it("draws and returns multiple line segments", function() {
+			svgCanvas.drawLine(1, 2, 5, 10);
+			svgCanvas.drawLine(20, 60, 2, 3);
+			svgCanvas.drawLine(0, 0, 100, 200);
+			expect(svgCanvas.lineSegments()).to.eql([
+				[1, 2, 5, 10],
+				[20, 60, 2, 3],
+				[0, 0, 100, 200]
+			]);
+		});
+
+	});
+
+}());
