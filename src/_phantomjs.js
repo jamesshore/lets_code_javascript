05021b9a26f5642826d1fef0cb787b31e0fa7bb9
James Shore <jshore@jamesshore.com>
2013-05-22 14:20:55 -0700

    Got PhantomJS smoke test working with require()

--- src/_phantomjs.js
+++ src/_phantomjs.js
@@ -29,12 +29,15 @@
 
 	function inBrowser() {
 		try {
-			var drawingArea = new wwp.HtmlElement($("#drawingArea"));
+			var client = require("./client.js");
+			var HtmlElement = require("./html_element.js");
+
+			var drawingArea = new HtmlElement($("#drawingArea"));
 			drawingArea.doMouseDown(10, 20);
 			drawingArea.doMouseMove(50, 60);
 			drawingArea.doMouseUp(50, 60);
 
-			var actual = JSON.stringify(wwp.drawingAreaCanvas.lineSegments());
+			var actual = JSON.stringify(client.drawingAreaCanvas.lineSegments());
 			var expected = JSON.stringify([[ "10", "20", "50", "60" ]]);
 
 			if (actual !== expected) return "lines drawn expected " + expected + " but was " + actual;
