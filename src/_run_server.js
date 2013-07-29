5c2ed9ac9bd7003e8926b2425796d79cf16bb087
James Shore <jshore@jamesshore.com>
2013-07-29 11:54:04 -0700

    Started factoring out code to start server (not done yet)

---
+++ src/_run_server.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+(function() {
+	"use strict";
+
+	exports.runServer = function(callback) {
+		var commandLine = parseProcFile();
+		var serverProcess = child_process.spawn(commandLine.command, commandLine.options, {stdio: ["pipe", "pipe", process.stderr]});
+		serverProcess.stdout.setEncoding("utf8");
+		serverProcess.stdout.on("data", function(chunk) {
+			if (chunk.trim().indexOf("Server started") !== -1) callback(serverProcess);
+		});
+	};
+
+	function parseProcFile() {
+		var fileData = fs.readFileSync("Procfile", "utf8");
+		var webCommand = procfile.parse(fileData).web;
+		webCommand.options = webCommand.options.map(function(element) {
+			if (element === "$PORT") return "5000";
+			else return element;
+		});
+		return webCommand;
+	}
+
+}());
