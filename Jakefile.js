a42b3a7dba456759f8adcdeb61c9d0d8f64d9cac
James Shore <jshore@jamesshore.com>
2013-07-29 12:19:01 -0700

    Added a bit of explanation to output of 'jake run'

--- Jakefile.js
+++ Jakefile.js
@@ -52,7 +52,9 @@
 
 	desc("Start WeeWikiPaint server for manual testing");
 	task("run", ["build"], function() {
+		console.log("Running server. Press Ctrl-C to stop.");
 		runServer.runInteractively();
+		// We never call complete() because we want the task to hang until the user presses 'Ctrl-C'.
 	}, {async: true});
 
 	desc("Lint everything");
