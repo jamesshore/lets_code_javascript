134a00f4b44b04843702ca8fcc57098d2b59eb9c
James Shore <jshore@jamesshore.com>
2013-09-18 13:32:18 -0700

    Replaced custom feature detection code with Modernizr library

--- src/client/browser.js
+++ src/client/browser.js
@@ -1,11 +1,11 @@
 // Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
-/*global $ */
+/*global Modernizr, $ */
 
 (function() {
 	"use strict";
 
 	exports.supportsTouchEvents = function() {
-		return (typeof Touch !== "undefined") && ('ontouchstart' in window);
+		return Modernizr.touch;
 	};
 
 	exports.reportsElementPositionOffByOneSometimes = function() {
