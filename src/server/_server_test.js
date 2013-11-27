7ad3318a46cc823d8754cd437668754a94fa58c8
James Shore <jshore@jamesshore.com>
2013-11-27 11:10:37 -0800

    Moved server code into EPERM test file

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,10 +2,12 @@
 (function () {
 	"use strict";
 
-	var server = require("./server.js");
 	var http = require("http");
 	var fs = require("fs");
 	var assert = require("assert");
+	var send = require("send");
+
+	var server;
 
 	var CONTENT_DIR = "generated/test";
 
@@ -18,38 +20,75 @@
 	var BASE_URL = "http://localhost:" + PORT;
 
 	exports.unifiedTestCase = function (test) {
-		var path = CONTENT_DIR + "/" + INDEX_PAGE;
+		startServer(CONTENT_DIR, NOT_FOUND_PAGE, PORT, function () {
 
-		fs.writeFileSync(path, INDEX_PAGE_DATA);
+			var path = CONTENT_DIR + "/" + INDEX_PAGE;
+			fs.writeFileSync(path, INDEX_PAGE_DATA);
 
-		httpGet(BASE_URL + "/" + INDEX_PAGE, function (response, responseData) {
-			fs.unlinkSync(path);
-//			fs.unlink(path, function() {
+			httpGet(BASE_URL + "/" + INDEX_PAGE, function (response, responseData) {
+				fs.unlinkSync(path);
+				//			fs.unlink(path, function() {
 				fs.writeFileSync(path, INDEX_PAGE_DATA);
 				test.done();
-//			});
+				//			});
+			});
+
+
 		});
 
 	};
 
 	function httpGet(url, callback) {
-		server.start(CONTENT_DIR, NOT_FOUND_PAGE, PORT, function () {
-			http.get(url, function (response) {
-				var receivedData = "";
-				response.setEncoding("utf8");
+		http.get(url, function (response) {
+			var receivedData = "";
+			response.setEncoding("utf8");
 
-				response.on("data", function (chunk) {
-					receivedData += chunk;
-				});
-				response.on("error", function (err) {
-					console.log("ERROR", err);
-				});
-				response.on("end", function () {
-					server.stop(function () {
-						callback(response, receivedData);
-					});
+			response.on("data", function (chunk) {
+				receivedData += chunk;
+			});
+			response.on("error", function (err) {
+				console.log("ERROR", err);
+			});
+			response.on("end", function () {
+				stopServer(function () {
+					callback(response, receivedData);
 				});
 			});
 		});
 	}
+
+
+
+
+	function startServer(contentDir, notFoundPageToServe, portNumber, callback) {
+		if (!portNumber) throw "port number is required";
+
+		server = http.createServer();
+		server.on("request", function(request, response) {
+			send(request, request.url).
+					root(contentDir).
+					on("error", handleError).
+					pipe(response);
+
+			function handleError(err) {
+				if (err.status === 404) serveErrorFile(response, 404, contentDir + "/" + notFoundPageToServe);
+				else throw err;
+			}
+		});
+		server.listen(portNumber, callback);
+	}
+
+	function stopServer(callback) {
+		server.close(callback);
+	}
+
+	function serveErrorFile(response, statusCode, file) {
+		response.statusCode = statusCode;
+		fs.readFile(file, function(err, data) {
+			if (err) throw err;
+			response.end(data);
+		});
+	}
+
+
 }());
