469c2dd385222095fe618615372f536096248d53
James Shore <jshore@jamesshore.com>
2013-11-27 10:19:13 -0800

    Created simplified test case for Windows 7 EPERM issue

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-(function() {
+(function () {
 	"use strict";
 
 	var server = require("./server.js");
@@ -20,114 +20,40 @@
 	var PORT = 5020;
 	var BASE_URL = "http://localhost:" + PORT;
 
-	exports.setUp = function(done) {
-		fs.writeFileSync(CONTENT_DIR + "/" + INDEX_PAGE, INDEX_PAGE_DATA);
-		fs.writeFileSync(CONTENT_DIR + "/" + OTHER_PAGE, OTHER_PAGE_DATA);
-		fs.writeFileSync(CONTENT_DIR + "/" + NOT_FOUND_PAGE, NOT_FOUND_DATA);
+	exports.unifiedTestCase = function (test) {
+		var path = CONTENT_DIR + "/" + INDEX_PAGE;
 
-		done();
-	};
+		fs.writeFileSync(path, INDEX_PAGE_DATA);
 
-	exports.tearDown = function(done) {
-		cleanUpFile(CONTENT_DIR + "/" + INDEX_PAGE);
-		cleanUpFile(CONTENT_DIR + "/" + OTHER_PAGE);
-		cleanUpFile(CONTENT_DIR + "/" + NOT_FOUND_PAGE);
-		done();
-	};
-
-	exports.test_servesFilesFromDirectory = function(test) {
-		httpGet(BASE_URL + "/" + INDEX_PAGE, function(response, responseData) {
-			test.equals(200, response.statusCode, "status code");
-			test.equals(INDEX_PAGE_DATA, responseData, "response text");
-			test.done();
+		httpGet(BASE_URL + "/" + INDEX_PAGE, function (response, responseData) {
+			fs.unlinkSync(path);
+			fs.writeFileSync(path, INDEX_PAGE_DATA);
+			httpGet(BASE_URL + "/" + INDEX_PAGE, function (response, responseData) {
+				fs.unlinkSync(path);
+				test.done();
+			});
 		});
-	};
 
-	exports.test_supportsMultipleFiles = function(test) {
-		httpGet(BASE_URL + "/" + OTHER_PAGE, function(response, responseData) {
-			test.equals(200, response.statusCode, "status code");
-			test.equals(OTHER_PAGE_DATA, responseData, "response text");
-			test.done();
-		});
-	};
-
-	exports.test_servesIndexDotHtmlWhenAskedForHomePage = function(test) {
-		httpGet(BASE_URL, function(response, responseData) {
-			test.equals(200, response.statusCode, "status code");
-			test.equals(INDEX_PAGE_DATA, responseData, "response text");
-			test.done();
-		});
-	};
-
-	exports.test_returns404WhenFileDoesNotExist = function(test) {
-		httpGet(BASE_URL + "/bargle", function(response, responseData) {
-			test.equals(404, response.statusCode, "status code");
-			test.equals(NOT_FOUND_DATA, responseData, "404 text");
-			test.done();
-		});
-	};
-
-	exports.test_requiresHomePageParameter = function(test) {
-		test.throws(function() {
-			server.start();
-		});
-		test.done();
-	};
-
-	exports.test_requires404PageParameter = function(test) {
-		test.throws(function() {
-			server.start(CONTENT_DIR);
-		});
-		test.done();
-	};
-
-	exports.test_requiresPortParameter = function(test) {
-		test.throws(function() {
-			server.start(CONTENT_DIR, NOT_FOUND_PAGE);
-		});
-		test.done();
-	};
-
-	exports.test_runsCallbackWhenStopCompletes = function(test) {
-		server.start(CONTENT_DIR, NOT_FOUND_PAGE, PORT);
-		server.stop(function() {
-			test.done();
-		});
-	};
-
-	exports.test_stopThrowsExceptionWhenNotRunning = function(test) {
-		test.throws(function() {
-			server.stop();
-		});
-		test.done();
 	};
 
 	function httpGet(url, callback) {
-		server.start(CONTENT_DIR, NOT_FOUND_PAGE, PORT, function() {
-			http.get(url, function(response) {
+		server.start(CONTENT_DIR, NOT_FOUND_PAGE, PORT, function () {
+			http.get(url, function (response) {
 				var receivedData = "";
 				response.setEncoding("utf8");
 
-				response.on("data", function(chunk) {
+				response.on("data", function (chunk) {
 					receivedData += chunk;
 				});
-				response.on("error", function(err) {
+				response.on("error", function (err) {
 					console.log("ERROR", err);
 				});
-				response.on("end", function() {
-					server.stop(function() {
+				response.on("end", function () {
+					server.stop(function () {
 						callback(response, receivedData);
 					});
 				});
 			});
 		});
 	}
-
-	function cleanUpFile(file) {
-		if (fs.existsSync(file)) {
-			fs.unlinkSync(file);
-			assert.ok(!fs.existsSync(file), "could not delete test file: [" + file + "]");
-		}
-	}
-
 }());
