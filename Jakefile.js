ff06331668ad4fe10fe8b23e6d430be68d3b7dfd
James Shore <jshore@jamesshore.com>
2014-01-12 13:48:25 -0800

    Updated Firefox 25.0.0 --> 26.0.0

--- Jakefile.js
+++ Jakefile.js
@@ -20,7 +20,7 @@
 	var REQUIRED_BROWSERS = [
 		"IE 8.0.0 (Windows 7)",
 		"IE 9.0.0 (Windows 7)",
-		"Firefox 25.0.0 (Mac OS X 10.8)",
+		"Firefox 26.0.0 (Mac OS X 10.8)",
 		"Chrome 31.0.1650 (Mac OS X 10.8.5)",
 		"Safari 6.1.0 (Mac OS X 10.8.5)",
 		"Mobile Safari 7.0.0 (iOS 7.0.3)"
