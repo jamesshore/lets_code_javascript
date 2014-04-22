40591837303f257eb7733cfb6d4891f0dc017a71
James Shore <jshore@jamesshore.com>
2014-04-22 11:36:56 -0700

    Updated Safari 6.1.2 --> 6.1.3

--- Jakefile.js
+++ Jakefile.js
@@ -22,7 +22,7 @@
 		"IE 9.0.0 (Windows 7)",
 		"Firefox 28.0.0 (Mac OS X 10.8)",
 		"Chrome 34.0.1847 (Mac OS X 10.8.5)",
-		"Safari 6.1.2 (Mac OS X 10.8.5)",
+		"Safari 6.1.3 (Mac OS X 10.8.5)",
 		"Mobile Safari 7.0.0 (iOS 7.1)"
 	];
 
