3157574c2a8abeb753eea4bda2f150a67ae4b6ae
James Shore <jshore@jamesshore.com>
2014-06-02 16:13:30 -0700

    Attempted to implement 'isElementCenteredOnPage'

--- src/client/_css_test.js
+++ src/client/_css_test.js
@@ -11,8 +11,8 @@
 		var htmlElement;
 
 		beforeEach(function() {
-			htmlElement = HtmlElement.fromHtml("<h1 style='text-align: center'>Hello World</h1>");
-//			htmlElement = HtmlElement.fromHtml("<h1 style='margin-left: auto; margin-right: auto; width: 200px;'>Hello World</h1>");
+//			htmlElement = HtmlElement.fromHtml("<h1 style='text-align: center'>Hello World</h1>");
+			htmlElement = HtmlElement.fromHtml("<h1 style='margin-left: auto; margin-right: auto; width: 200px;'>Hello World</h1>");
 			htmlElement.appendSelfToBody();
 		});
 
@@ -21,8 +21,8 @@
 		});
 
 		it("headline is centered", function(done) {
-			expect(isTextCentered(htmlElement)).to.be(true);
-//			expect(isElementCentered(htmlElement)).to.be(true);
+//			expect(isTextCentered(htmlElement)).to.be(true);
+			expect(isElementCenteredInPage(htmlElement)).to.be(true);
 
 //			setTimeout(done, 5000);
 			done();
@@ -37,6 +37,37 @@
 		var textAlign = style.getPropertyValue("text-align");
 
 		return textAlign === "center";
+	}
+
+	function isElementCenteredInPage(element) {
+		var domElement = element.toDomElement();
+
+		var boundingBox = domElement.getBoundingClientRect();
+		var elementWidth = boundingBox.width;
+		var elementLeft = boundingBox.left;
+		var elementRight = boundingBox.right;
+
+		dump(elementWidth, elementLeft, elementRight);
+
+//		dump(document.body.getBoundingClientRect());
+
+		var bodyWidth = document.body.clientWidth;
+
+		var expectedSides = (bodyWidth - elementWidth) / 2;
+
+		var success = true;
+		if (elementLeft !== expectedSides) {
+			console.log("expected left to be " + expectedSides + " but was " + elementLeft + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
+			success = false;
+		}
+
+		var rightSide = bodyWidth - elementRight;
+		if (rightSide !== expectedSides) {
+			console.log("expected right to be " + expectedSides + " but was " + elementRight + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
+			success = false;
+		}
+
+		return success;
 	}
 
 }());
