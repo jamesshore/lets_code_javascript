a5cd633ddb451e414df5867e6cd896f152de7d57
James Shore <jshore@jamesshore.com>
2014-07-21 11:13:13 -0700

    Cleaned up element creation in CSS test

--- src/client/_css_test.js
+++ src/client/_css_test.js
@@ -8,75 +8,84 @@
 	describe("Home page", function() {
 		if (browser.doesNotComputeStyles()) return;
 
+		var logo;
+		var tagline;
+
+		beforeEach(function() {
+			logo = newElement("<h1 id='logo'>Hello World</h1>");
+			tagline = newElement("<p id='tagline'>Tag line here</p>");
+		});
+
+		afterEach(function() {
+			logo.remove();
+			tagline.remove();
+		});
+
+		function newElement(html) {
+			var element = HtmlElement.fromHtml(html);
+			element.appendSelfToBody();
+			return element;
+		}
+
 		it("has a blue background", function() {
 			expect(backgroundColorOf(document.body)).to.be("rgb(66, 169, 204)");
 		});
 
 		it("centers logo at top of page", function() {
-			expect(isElementCenteredInPage("<h1 id='logo' style='width: 200px;'>Hello World</h1>")).to.be(true);
-			expect(isTextCentered("<h1 id='logo' style='width: 200px;'>Hello World</h1>")).to.be(true);
+			expect(isElementCenteredInPage(logo)).to.be(true);
+			expect(isTextCentered(logo)).to.be(true);
 
-			expect(elementPixelsFromTopOfPage("<h1 id='logo' style='width: 200px;'>Hello World</h1>")).to.be(12);
+			expect(elementPixelsFromTopOfPage(logo)).to.be(12);
 		});
 
 		it("centers tagline directly below logo", function() {
-			expect(isTextCentered("<p id='tagline'>Tag line here</p>")).to.be(true);
+			expect(isTextCentered(tagline)).to.be(true);
+
+//			expect(elementPixelsBelowElement(tagline, logo)).to.be(8);
+
+
 		});
 
 	});
 
-	function isElementCenteredInPage(html) {
-		var element = HtmlElement.fromHtml(html);
-		element.appendSelfToBody();
-		try {
-			var domElement = element.toDomElement();
+	function isElementCenteredInPage(element) {
+		var domElement = element.toDomElement();
 
-			var boundingBox = domElement.getBoundingClientRect();
-			var elementWidth = boundingBox.width;
-			var elementLeft = boundingBox.left;
-			var elementRight = boundingBox.right;
+		var boundingBox = domElement.getBoundingClientRect();
+		var elementWidth = boundingBox.width;
+		var elementLeft = boundingBox.left;
+		var elementRight = boundingBox.right;
 
-			var bodyStyle = window.getComputedStyle(document.body);
+		var bodyStyle = window.getComputedStyle(document.body);
 
-			var bodyWidthExcludingMargins = document.body.clientWidth;
-			var bodyLeftMarginWidth = pixelsToInt(bodyStyle.getPropertyValue("margin-left"));
-			var bodyRightMarginWidth = pixelsToInt(bodyStyle.getPropertyValue("margin-right"));
-			var bodyWidth = bodyWidthExcludingMargins + bodyLeftMarginWidth + bodyRightMarginWidth;
+		var bodyWidthExcludingMargins = document.body.clientWidth;
+		var bodyLeftMarginWidth = pixelsToInt(bodyStyle.getPropertyValue("margin-left"));
+		var bodyRightMarginWidth = pixelsToInt(bodyStyle.getPropertyValue("margin-right"));
+		var bodyWidth = bodyWidthExcludingMargins + bodyLeftMarginWidth + bodyRightMarginWidth;
 
-			var expectedSides = (bodyWidth - elementWidth) / 2;
+		var expectedSides = (bodyWidth - elementWidth) / 2;
 
-			var success = true;
-			if (elementLeft !== Math.round(expectedSides)) {
-				console.log("expected left to be " + expectedSides + " but was " + elementLeft + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
-				success = false;
-			}
-
-			var expectedRight = Math.round(bodyWidth - expectedSides);
-			if (elementRight !== expectedRight) {
-				console.log("expected right to be " + expectedRight + " but was " + elementRight + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
-				success = false;
-			}
-
-			return success;
+		var success = true;
+		if (elementLeft !== Math.round(expectedSides)) {
+			console.log("expected left to be " + expectedSides + " but was " + elementLeft + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
+			success = false;
 		}
-		finally {
-			element.remove();
+
+		var expectedRight = Math.round(bodyWidth - expectedSides);
+		if (elementRight !== expectedRight) {
+			console.log("expected right to be " + expectedRight + " but was " + elementRight + " (element is " + elementWidth + "px wide; screen is " + bodyWidth + "px wide)");
+			success = false;
 		}
+
+		return success;
 	}
 
-	function elementPixelsFromTopOfPage(html) {
-		var element = HtmlElement.fromHtml(html);
-		element.appendSelfToBody();
-		try {
-			var domElement = element.toDomElement();
+	function elementPixelsFromTopOfPage(element) {
+		var domElement = element.toDomElement();
 
-			var boundingBox = domElement.getBoundingClientRect();
+		var boundingBox = domElement.getBoundingClientRect();
 
-			return boundingBox.top;
-		}
-		finally {
-			element.remove();
-		}
+		return boundingBox.top;
 	}
 
 	function backgroundColorOf(domElement) {
@@ -84,20 +93,13 @@
 		return style.getPropertyValue("background-color");
 	}
 
-	function isTextCentered(html) {
-		var element = HtmlElement.fromHtml(html);
-		element.appendSelfToBody();
-		try {
-			var domElement = element.toDomElement();
+	function isTextCentered(element) {
+		var domElement = element.toDomElement();
 
-			var style = window.getComputedStyle(domElement);
-			var textAlign = style.getPropertyValue("text-align");
+		var style = window.getComputedStyle(domElement);
+		var textAlign = style.getPropertyValue("text-align");
 
-			return textAlign === "center";
-		}
-		finally {
-			element.remove();
-		}
+		return textAlign === "center";
 	}
 
 	function pixelsToInt(pixels) {
