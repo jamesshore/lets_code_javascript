0ed77b85b24f3fffd90f6c3e6464a66cf8338a59
James Shore <jshore@jamesshore.com>
2014-07-21 10:51:20 -0700

    Upgraded Safari 6.1.4 --> 6.1.5

--- Jakefile.js
+++ Jakefile.js
@@ -22,7 +22,7 @@
 		"IE 9.0.0 (Windows 7)",
 		"Firefox 30.0.0 (Mac OS X 10.8)",
 		"Chrome 35.0.1916 (Mac OS X 10.8.5)",
-		"Safari 6.1.4 (Mac OS X 10.8.5)",
+		"Safari 6.1.5 (Mac OS X 10.8.5)",
 		"Mobile Safari 7.0.0 (iOS 7.1)"
 	];
 
