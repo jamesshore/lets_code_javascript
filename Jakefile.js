18b870181286f7a41afc09b1f4408f5343bc5101
James Shore <jshore@jamesshore.com>
2014-09-16 12:50:22 -0700

    Updated Firefox 31 --> 32

--- Jakefile.js
+++ Jakefile.js
@@ -20,7 +20,7 @@
 	var REQUIRED_BROWSERS = [
 		"IE 8.0.0 (Windows 7)",
 		"IE 9.0.0 (Windows 7)",
-		"Firefox 31.0.0 (Mac OS X 10.8)",
+		"Firefox 32.0.0 (Mac OS X 10.8)",
 		"Chrome 37.0.2062 (Mac OS X 10.8.5)",
 		"Safari 6.1.6 (Mac OS X 10.8.5)",
 		"Mobile Safari 7.0.0 (iOS 7.1)"
