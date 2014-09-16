dbacba1f4c5a382db96b92da06e00ce2a1f4d69a
James Shore <jshore@jamesshore.com>
2014-09-16 14:00:50 -0700

    Set aside styling #drawingArea for now

--- src/client/_css_test.js
+++ src/client/_css_test.js
@@ -42,7 +42,7 @@
 				style.toDomElement().addEventListener("load", function() {
 					logo = newElement("<h1 id='logo'>Hello World</h1>");
 					tagline = newElement("<p id='tagline'>Tag line here</p>");
-					drawingArea = newElement("<div id='drawingArea'></div>");
+//					drawingArea = newElement("<div id='drawingArea'>Foo!</div>");
 //					drawingAreaContainer = newElement("" +
 //						"<div id='drawingAreaContainer'>" +
 //						" <div id='drawingArea'></div>" +
@@ -64,7 +64,7 @@
 		});
 
 		afterEach(function() {
-			frame.remove();
+//			frame.remove();
 		});
 
 		function newElement(html) {
@@ -109,14 +109,14 @@
 			expect(textColorOf(tagline)).to.be(DARK_BLUE);
 		});
 
-		it("centers drawing area below tagline", function() {
-			expect(isElementCenteredInPage(drawingArea)).to.be(true);
-			expect(elementPixelsBelowElement(drawingArea, tagline)).to.be(10);
-
+//		it.only("centers drawing area below tagline", function() {
+//			expect(isElementCenteredInPage(drawingArea)).to.be(true);
+//			expect(elementPixelsBelowElement(drawingArea, tagline)).to.be(10);
+//
 //			expect(elementHeightInPixels(drawingArea)).to.equal(600);
 //			expect(backgroundColorOf(drawingArea)).to.equal(WHITE);
 //			expect(roundedCornersOf(drawingArea)).to.be(CORNER_ROUNDING);
-		});
+//		});
 
 //		it("centers an arrow at top of drawing area", function() {
 //			expect(isElementCenteredInPage(drawingAreaArrow)).to.be(true);
@@ -375,7 +375,7 @@
 		}
 
 		function getComputedProperty(element, propertyName) {
-			var style = frame.toDomElement().contentWindow.getComputedStyle(element.toDomElement());
+			var style = window.getComputedStyle(element.toDomElement());
 			return style.getPropertyValue(propertyName);
 		}
 
