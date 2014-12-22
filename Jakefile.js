d32d6cd6a71dbe14b370f0bcc1938348b0fa8a54
James Shore <jshore@jamesshore.com>
2014-12-22 14:42:04 -0800

    Upgrade Safari 6.2.0 --> 6.2.2

--- Jakefile.js
+++ Jakefile.js
@@ -22,7 +22,7 @@
 		"IE 9.0.0 (Windows 7)",
 		"Firefox 33.0.0 (Mac OS X 10.8)",
 		"Chrome 39.0.2171 (Mac OS X 10.8.5)",
-		"Safari 6.2.0 (Mac OS X 10.8.5)",
+		"Safari 6.2.2 (Mac OS X 10.8.5)",
 		"Mobile Safari 7.0.0 (iOS 7.1)"
 	];
 
