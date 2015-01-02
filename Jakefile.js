6822bb1dab2b193392f5864abb578821217d881b
James Shore <jshore@jamesshore.com>
2015-01-02 13:46:48 -0800

    Upgraded Mac OS 10.8 --> 10.10.1

--- Jakefile.js
+++ Jakefile.js
@@ -20,9 +20,9 @@
 	var REQUIRED_BROWSERS = [
 		"IE 8.0.0 (Windows 7)",
 		"IE 9.0.0 (Windows 7)",
-		"Firefox 34.0.0 (Mac OS X 10.8)",
-		"Chrome 39.0.2171 (Mac OS X 10.8.5)",
-		"Safari 6.2.2 (Mac OS X 10.8.5)",
+		"Firefox 34.0.0 (Mac OS X 10.10)",
+		"Chrome 39.0.2171 (Mac OS X 10.10.1)",
+		"Safari 6.2.2 (Mac OS X 10.10.1)",
 		"Mobile Safari 7.0.0 (iOS 7.1)"
 	];
 
