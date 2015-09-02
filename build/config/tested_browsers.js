6900abb3b61bc32d9abde75658fcf40bdbc31e61
James Shore <jshore@jamesshore.com>
2015-09-02 12:09:14 -0700

    Upgraded Safari 8.0.7 --> 8.0.8

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -7,7 +7,7 @@
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Firefox 40.0.0 (Mac OS X 10.10.0)",
 		"Chrome 45.0.2454 (Mac OS X 10.10.5)",
-		"Safari 8.0.7 (Mac OS X 10.10.5)",
+		"Safari 8.0.8 (Mac OS X 10.10.5)",
 		"Mobile Safari 8.0.0 (iOS 8.4.0)",
 		"Chrome Mobile 39.0.0 (Android 5.1.0)"
 	];
