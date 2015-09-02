a2e3cc9352ff44b3247da492e8df5753aad004af
James Shore <jshore@jamesshore.com>
2015-09-02 12:06:29 -0700

    Upgraded Chrome 44 --> 45

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 9.0.0 (Windows 7 0.0.0)",
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Firefox 40.0.0 (Mac OS X 10.10.0)",
-		"Chrome 44.0.2403 (Mac OS X 10.10.4)",
+		"Chrome 45.0.2454 (Mac OS X 10.10.4)",
 		"Safari 8.0.7 (Mac OS X 10.10.4)",
 		"Mobile Safari 8.0.0 (iOS 8.4.0)",
 		"Chrome Mobile 39.0.0 (Android 5.1.0)"
