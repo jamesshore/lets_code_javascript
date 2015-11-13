b330ef586d1f2a6fbe7fd50209035798f576e5f0
James Shore <jshore@jamesshore.com>
2015-11-13 14:49:09 -0800

    Upgraded Firefox 41 --> 42

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 9.0.0 (Windows 7 0.0.0)",
 		"IE 11.0.0 (Windows 7 0.0.0)",
-		"Firefox 41.0.0 (Mac OS X 10.10.0)",
+		"Firefox 42.0.0 (Mac OS X 10.10.0)",
 		"Chrome 45.0.2454 (Mac OS X 10.10.5)",
 		"Safari 9.0.0 (Mac OS X 10.10.5)",
 		"Mobile Safari 8.0.0 (iOS 8.4.0)",
