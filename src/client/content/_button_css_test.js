0503afe6c4cf70e576674ffce157e74d07a39c36
James Shore <jshore@jamesshore.com>
2015-11-17 16:02:14 -0800

    Restored missing button tests

--- src/client/content/_button_css_test.js
+++ src/client/content/_button_css_test.js
@@ -16,10 +16,10 @@
 
 		beforeEach(function() {
 			cssHelper.frame.add(
-				"<div style='font-family: " + INHERITED_FONT + "'>" +
-				" <a id='a_tag' class='button' href='#createUnderline'>foo</a>" +
-				" <button id='button_tag' class='button'>foo</button>" +
-				"</div>"
+					"<div style='font-family: " + INHERITED_FONT + "'>" +
+					" <a id='a_tag' class='button' href='#createUnderline'>foo</a>" +
+					" <button id='button_tag' class='button'>foo</button>" +
+					"</div>"
 			);
 
 			linkTag = cssHelper.frame.get("#a_tag");
@@ -51,67 +51,67 @@
 			cssHelper.assertActivateDepresses(linkTag, 1);
 		});
 
-	});
+		describe("Action button block variant", function() {
 
-	describe("Action button block variant", function() {
+			var linkTag;
+			var buttonTag;
 
-		var linkTag;
-		var buttonTag;
-
-		beforeEach(function() {
-			linkTag = cssHelper.frame.add("<a class='button button--action' href='#createUnderline'>foo</a>", "<a> button");
-			buttonTag = cssHelper.frame.add("<button class='button button--action'>foo</button>", "<button> button");
-		});
-
-		it("is big and pressable", function() {
-			linkTag.assert({
-				height: 35
+			beforeEach(function() {
+				linkTag = cssHelper.frame.add("<a class='button button--action' href='#createUnderline'>foo</a>", "<a> button");
+				buttonTag = cssHelper.frame.add("<button class='button button--action'>foo</button>", "<button> button");
 			});
-		});
 
-		it("has large text", function() {
-			assert.equal(cssHelper.isTextVerticallyCentered(linkTag), true, "should be vertically centered");
-			assert.equal(cssHelper.fontSize(linkTag), "16px", "font size");
-			assert.equal(cssHelper.fontWeight(linkTag), cssHelper.LINK_BUTTON_WEIGHT, "button weight");
-		});
-
-		it("uses bright colors", function() {
-			assert.equal(cssHelper.backgroundColor(linkTag), cssHelper.MEDIUM_BLUE, "background");
-			assert.equal(cssHelper.textColor(linkTag), cssHelper.WHITE, "text");
-			assert.equal(cssHelper.dropShadow(linkTag), cssHelper.DARK_BLUE + cssHelper.BUTTON_DROP_SHADOW, "drop shadow");
-			cssHelper.assertHoverStyle(linkTag, cssHelper.DARKENED_MEDIUM_BLUE, "hover background");
-		});
-
-	});
-
-
-	describe("Drawing button block variant", function() {
-
-		var linkTag;
-		var buttonTag;
-
-		beforeEach(function() {
-			linkTag = cssHelper.frame.add("<a class='button button--drawing' href='#createUnderline'>foo</a>", "<a> button");
-			buttonTag = cssHelper.frame.add("<button class='button button--drawing'>foo</button>", "<button> button");
-		});
-
-		it("is a bit smaller", function() {
-			linkTag.assert({
-				height: 30
+			it("is big and pressable", function() {
+				linkTag.assert({
+					height: 35
+				});
 			});
+
+			it("has large text", function() {
+				assert.equal(cssHelper.isTextVerticallyCentered(linkTag), true, "should be vertically centered");
+				assert.equal(cssHelper.fontSize(linkTag), "16px", "font size");
+				assert.equal(cssHelper.fontWeight(linkTag), cssHelper.LINK_BUTTON_WEIGHT, "button weight");
+			});
+
+			it("uses bright colors", function() {
+				assert.equal(cssHelper.backgroundColor(linkTag), cssHelper.MEDIUM_BLUE, "background");
+				assert.equal(cssHelper.textColor(linkTag), cssHelper.WHITE, "text");
+				assert.equal(cssHelper.dropShadow(linkTag), cssHelper.DARK_BLUE + cssHelper.BUTTON_DROP_SHADOW, "drop shadow");
+				cssHelper.assertHoverStyle(linkTag, cssHelper.DARKENED_MEDIUM_BLUE, "hover background");
+			});
+
 		});
 
-		it("has smaller, bolder text", function() {
-			assert.equal(cssHelper.fontSize(linkTag), "12px", "font size");
-			assert.equal(cssHelper.fontWeight(linkTag), cssHelper.DRAWING_BUTTON_WEIGHT, "font weight");
-			assert.equal(cssHelper.isTextVerticallyCentered(linkTag), true, "should be vertically centered");
-		});
 
-		it("uses muted colors", function() {
-			assert.equal(cssHelper.backgroundColor(linkTag), cssHelper.GRAY, "button background");
-			assert.equal(cssHelper.textColor(linkTag), cssHelper.DARK_GRAY, "button text");
-			assert.equal(cssHelper.dropShadow(linkTag), cssHelper.MEDIUM_GRAY + cssHelper.BUTTON_DROP_SHADOW, "drop shadow");
-			cssHelper.assertHoverStyle(linkTag, cssHelper.DARKENED_GRAY, "hover background");
+		describe("Drawing button block variant", function() {
+
+			var linkTag;
+			var buttonTag;
+
+			beforeEach(function() {
+				linkTag = cssHelper.frame.add("<a class='button button--drawing' href='#createUnderline'>foo</a>", "<a> button");
+				buttonTag = cssHelper.frame.add("<button class='button button--drawing'>foo</button>", "<button> button");
+			});
+
+			it("is a bit smaller", function() {
+				linkTag.assert({
+					height: 30
+				});
+			});
+
+			it("has smaller, bolder text", function() {
+				assert.equal(cssHelper.fontSize(linkTag), "12px", "font size");
+				assert.equal(cssHelper.fontWeight(linkTag), cssHelper.DRAWING_BUTTON_WEIGHT, "font weight");
+				assert.equal(cssHelper.isTextVerticallyCentered(linkTag), true, "should be vertically centered");
+			});
+
+			it("uses muted colors", function() {
+				assert.equal(cssHelper.backgroundColor(linkTag), cssHelper.GRAY, "button background");
+				assert.equal(cssHelper.textColor(linkTag), cssHelper.DARK_GRAY, "button text");
+				assert.equal(cssHelper.dropShadow(linkTag), cssHelper.MEDIUM_GRAY + cssHelper.BUTTON_DROP_SHADOW, "drop shadow");
+				cssHelper.assertHoverStyle(linkTag, cssHelper.DARKENED_GRAY, "hover background");
+			});
+
 		});
 
 	});
