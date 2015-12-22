4540021a4a69df1b7b3525f8a233709ae9428ce4
James Shore <jshore@jamesshore.com>
2015-12-21 16:41:23 -0800

    Upgraded Chrome 46 --> 47

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 9.0.0 (Windows 7 0.0.0)",
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Firefox 43.0.0 (Mac OS X 10.10.0)",
-		"Chrome 46.0.2490 (Mac OS X 10.10.5)",
+		"Chrome 47.0.2526 (Mac OS X 10.10.5)",
 		"Safari 9.0.1 (Mac OS X 10.10.5)",
 		"Mobile Safari 9.0.0 (iOS 9.1.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
