62497ec61ed98d0f8fafa3669ee7c8c2a728f226
James Shore <jshore@jamesshore.com>
2015-12-30 17:33:27 -0800

    Replaced IE 9 with MS Edge

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -3,8 +3,8 @@
 	"use strict";
 
 	module.exports = [
-		"IE 9.0.0 (Windows 7 0.0.0)",
 		"IE 11.0.0 (Windows 7 0.0.0)",
+		"Edge 13.10586.0 (Windows 10 0.0.0)",
 		"Firefox 43.0.0 (Mac OS X 10.10.0)",
 		"Chrome 47.0.2526 (Mac OS X 10.10.5)",
 		"Safari 9.0.2 (Mac OS X 10.10.5)",
