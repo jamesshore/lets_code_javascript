5fcde23b5c4f415a94ea4acd9b81b41c9fb8edee
James Shore <jshore@jamesshore.com>
2016-02-14 13:43:34 -0800

    Created bare-bones network abstraction

--- src/client/network/_real_time_network_test.js
+++ src/client/network/_real_time_network_test.js
@@ -1,22 +1,29 @@
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
-/* global io:false */
-
 (function() {
 	"use strict";
 
 	var assert = require("../../shared/_assert.js");
 	var harness = require("./_network_test_harness.js");
+	var network = require("./real_time_network.js");
 
 	describe("NET: Real Time Network", function() {
 
 		it("connects to Socket.IO server", function(done) {
-			var origin = window.location.protocol + "//" + window.location.hostname + ":" + harness.PORT;
-			var socket = io(origin);
 
-			socket.on("connect", function() {
-				socket.disconnect();
+			network.connect(harness.PORT, function() {
 				done();
 			});
+
+
+
+
+			//var origin = window.location.protocol + "//" + window.location.hostname + ":" + harness.PORT;
+			//var socket = io(origin);
+			//
+			//socket.on("connect", function() {
+			//	socket.disconnect();
+			//	done();
+			//});
 		});
 
 	});
---
+++ src/client/network/real_time_network.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+/* global io:false */
+(function() {
+	"use strict";
+
+	exports.connect = function(port, callback) {
+		var origin = window.location.protocol + "//" + window.location.hostname + ":" + port;
+		var socket = io(origin);
+
+		socket.on("connect", function() {
+			callback();
+		});
+	};
+
+}());
