4649c1ccd8eedc00c88a484fc0645c296781391d
James Shore <jshore@jamesshore.com>
2016-04-26 11:25:34 -0700

    Upgrade iOS 9.2 --> 9.3

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -8,7 +8,7 @@
 		"Firefox 45.0.0 (Mac OS X 10.11.0)",
 		"Chrome 50.0.2661 (Mac OS X 10.11.4)",
 		"Safari 9.1.0 (Mac OS X 10.11.4)",
-		"Mobile Safari 9.0.0 (iOS 9.2.0)",
+		"Mobile Safari 9.0.0 (iOS 9.3.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 	];
 
