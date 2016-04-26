5cc98bc9b98ee89faebd509e240c42eb2c34b959
James Shore <jshore@jamesshore.com>
2016-04-26 11:45:45 -0700

    Updated assertion library by copying from Quixote

--- src/shared/_assert.js
+++ src/shared/_assert.js
@@ -1,18 +1,60 @@
-// Copyright (c) 2014-2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+// Copyright (c) 2014-2016 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 (function() {
 	"use strict";
 
+	// ****
+	// An assertion library that works the way *I* want it to. <oldmanvoice>Get off my lawn!</oldmanvoice>
+	// ****
+
 	// We use Proclaim rather than Chai because Chai doesn't support IE 8.
 	// But Proclaim error messages are terrible, so we end up doing a lot ourselves.
-	var assert = require("./vendor/proclaim-2.0.0.js");
+	var proclaim = require("./vendor/proclaim-2.0.0.js");
+
+	var shim = {
+		Function: {
+			// WORKAROUND IE 8, IE 9, IE 10, IE 11: no function.name
+			name: function name(fn) {
+				if (fn.name) return fn.name;
+
+				// Based on code by Jason Bunting et al, http://stackoverflow.com/a/332429
+				var funcNameRegex = /function\s+(.{1,})\s*\(/;
+				var results = (funcNameRegex).exec((fn).toString());
+				return (results && results.length > 1) ? results[1] : "<anon>";
+			}
+		}
+	};
 
 	exports.fail = function(message) {
-		assert.fail(null, null, message);
+		proclaim.fail(null, null, message);
 	};
 
 	exports.defined = function(value, message) {
 		message = message ? message + ": " : "";
-		assert.isDefined(value, message + "expected any value, but was undefined");
+		proclaim.isDefined(value, message + "expected any value, but was undefined");
+	};
+
+	exports.undefined = function(value, message) {
+		message = message ? message + ": " : "";
+		proclaim.isUndefined(value, message + "expected undefined, but was '" + value + "'");
+	};
+
+	exports.type = function(obj, expectedType, message) {
+		message = message ? message + ": " : "";
+		proclaim.isInstanceOf(
+			obj,
+			expectedType,
+			message + "expected object to be instance of " +
+				shim.Function.name(expectedType) + ", but was " + describeObject(obj)
+		);
+	};
+
+	exports.implements = function(obj, expectedType, message) {
+		exports.type(obj, expectedType, message);
+
+		message = message ? message + ": " : "";
+		proclaim.isDefined(obj.checkAbstractMethods, message + describeObject(obj) + " does not extend an abstract class");
+		var needed = obj.checkAbstractMethods();
+		proclaim.isTrue(needed.length === 0, message + "must implement " + needed.join(" and "));
 	};
 
 	exports.equal = function(actual, expected, message) {
@@ -20,36 +62,49 @@
 		var expectedType = typeof expected;
 		var actualType = typeof actual;
 
-		assert.strictEqual(
-			actualType,
-			expectedType,
-			message + "expected " + expectedType + " '" + expected + "', but got " + actualType + " '" + actual + "'"
-		);
-		assert.strictEqual(
-			actual,
-			expected,
-			message + "expected '" + expected + "', but got '" + actual + "'"
-		);
+		if (actual !== undefined && expected !== undefined) {
+			proclaim.strictEqual(
+				actualType, expectedType,
+				message + "expected " + expectedType + " '" + expected + "', but got " + actualType + " '" + actual + "'"
+			);
+		}
+		proclaim.strictEqual(actual, expected, message + "expected '" + expected + "', but got '" + actual + "'");
+	};
+
+	exports.notEqual = function(actual, expected, message) {
+		message = message ? message + ": " : "";
+
+		proclaim.notEqual(actual, expected, message + "expected '" + expected + "' to be different from '" + actual + "'");
+	};
+
+	exports.lte = function(actual, expected, message) {
+		message = message ? message + ": " : "";
+
+		proclaim.isTrue(actual <= expected, message + "expected <= '" + expected + "', but got '" + actual + "'");
+	};
+
+	exports.objEqual = function(actual, expected, message) {
+		message = message ? message + ": " : "";
+		proclaim.isDefined(actual, message + "expected object, but was undefined");
+		proclaim.isTrue(actual.equals(expected), message + "object equality expected '" + expected + "', but got '" + actual + "'");
+	};
+
+	exports.objNotEqual = function(actual, expected, message) {
+		message = message ? message + ": " : "";
+		proclaim.isFalse(actual.equals(expected), message + "expected '" + expected + "' and '" + actual + "' to be not be equal(), but they were");
 	};
 
 	exports.deepEqual = function(actual, expected, message) {
 		message = message ? message + ": " : "";
-		assert.deepEqual(
-			actual,
-			expected,
-			message + "expected " + JSON.stringify(expected) + ", but got " + JSON.stringify(actual));
+		proclaim.deepEqual(actual, expected, message + "expected deep equality.");
 	};
 
-	exports.matches = function(actual, expectedRegex, message) {
+	exports.match = function(actual, expectedRegex, message) {
 		message = message ? message + ": " : "";
-		assert.match(
-			actual,
-			expectedRegex,
-			message + "expected string to match " + expectedRegex + ", but got '" + actual + "'"
-		);
+		proclaim.match(actual, expectedRegex, message + "expected string to match " + expectedRegex + ", but got '" + actual + "'");
 	};
 
-	exports.doesNotThrow = function(fn, message) {
+	exports.noException = function(fn, message) {
 		try {
 			fn();
 		}
@@ -59,7 +114,7 @@
 		}
 	};
 
-	exports.throws = function(fn, expected, message) {
+	exports.exception = function(fn, expected, message) {
 		message = message ? message + ": " : "";
 		var noException = false;
 		try {
@@ -68,20 +123,36 @@
 		}
 		catch (e) {
 			if (typeof expected === "string") {
-				assert.strictEqual(
+				proclaim.strictEqual(
 					e.message,
 					expected,
 					message + "expected exception message to be '" + expected + "', but was '" + e.message + "'"
 				);
 			}
-			else if (expected instanceof RegExp) assert.match(
+			else if (expected instanceof RegExp) proclaim.match(
 				e.message,
 				expected,
 				message + "expected exception message to match " + expected + ", but was '" + e.message + "'"
+			);
+			else if (typeof expected === "function") proclaim.isInstanceOf(
+				e,
+				expected,
+				message + "expected exception to be of type " + shim.Function.name(expected) + ", but was " + describeObject(e)
 			);
 			else if (expected !== undefined) throw new Error("Unrecognized 'expected' parameter in assertion: " + expected);
 		}
 		if (noException) exports.fail(message + "expected exception");
 	};
+
+	exports.throws = exports.exception;
+	exports.doesNotThrow = exports.noException;
+
+	function describeObject(obj) {
+		var actualType = "unknown";
+		var prototype = shim.Object.getPrototypeOf(obj);
+		if (prototype === null) actualType = "object without a prototype";
+		else if (prototype.constructor) actualType = shim.Function.name(prototype.constructor);
+		return actualType;
+	}
 
 }());
