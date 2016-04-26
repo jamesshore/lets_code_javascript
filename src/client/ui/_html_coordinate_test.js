dd0f8801ff201ec3dc2741f006ee7ee8bc6d2f2e
James Shore <jshore@jamesshore.com>
2016-04-26 13:13:46 -0700

    HtmlCoordinate converts page offsets to relative offsets

--- src/client/ui/_html_coordinate_test.js
+++ src/client/ui/_html_coordinate_test.js
@@ -20,49 +20,55 @@
 		});
 
 		it("provides relative x and y coordinates", function() {
-			var coord = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
+			var coord = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
 			assert.equal(coord.toRelativeX(), 10, "x");
 			assert.equal(coord.toRelativeY(), 20, "y");
 		});
 
+		it("converts page offsets to relative coordinates", function() {
+			var coord = HtmlCoordinate.fromPageOffset(element, 10, 20);
+			assert.equal(coord.toRelativeX(), 2, "x");
+			assert.equal(coord.toRelativeY(), 12, "y");
+		});
+
 		it("converts to string for debugging purposes", function() {
-			var coord = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
+			var coord = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
 			assert.equal(coord.toString(), "[HtmlCoordinate (10, 20) relative to <div>]");
 		});
 
 		describe("equality", function() {
 
 			it("is equal when based on the same data", function() {
-				var coord1 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
-				var coord2 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
+				var coord1 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
+				var coord2 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
 
 				assert.objEqual(coord1, coord2);
 			});
 
 			it("is equal when HtmlElement is different but the underlying DOM element is the same", function() {
-				var coord1 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
-				var coord2 = HtmlCoordinate.fromRelativeCoords(HtmlElement.fromId("element"), 10, 20);
+				var coord1 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
+				var coord2 = HtmlCoordinate.fromRelativeOffset(HtmlElement.fromId("element"), 10, 20);
 
 				assert.objEqual(coord1, coord2);
 			});
 
 			it("is not equal when elements are different", function() {
-				var coord1 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
-				var coord2 = HtmlCoordinate.fromRelativeCoords(HtmlElement.fromHtml("<div></div>"), 10, 20);
+				var coord1 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
+				var coord2 = HtmlCoordinate.fromRelativeOffset(HtmlElement.fromHtml("<div></div>"), 10, 20);
 
 				assert.objNotEqual(coord1, coord2);
 			});
 
 			it("is not equal when x values are different", function() {
-				var coord1 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
-				var coord2 = HtmlCoordinate.fromRelativeCoords(element, 15, 20);
+				var coord1 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
+				var coord2 = HtmlCoordinate.fromRelativeOffset(element, 15, 20);
 
 				assert.objNotEqual(coord1, coord2);
 			});
 
 			it("is not equal when y values are different", function() {
-				var coord1 = HtmlCoordinate.fromRelativeCoords(element, 10, 20);
-				var coord2 = HtmlCoordinate.fromRelativeCoords(element, 10, 25);
+				var coord1 = HtmlCoordinate.fromRelativeOffset(element, 10, 20);
+				var coord2 = HtmlCoordinate.fromRelativeOffset(element, 10, 25);
 
 				assert.objNotEqual(coord1, coord2);
 			});
--- src/client/ui/html_coordinate.js
+++ src/client/ui/html_coordinate.js
@@ -10,8 +10,13 @@
 		this._relativeY = relativeY;
 	};
 
-	HtmlCoordinate.fromRelativeCoords = function(element, x, y) {
+	HtmlCoordinate.fromRelativeOffset = function(element, x, y) {
 		return new HtmlCoordinate(element, x, y);
+	};
+
+	HtmlCoordinate.fromPageOffset = function(element, x, y) {
+		var relativeCoords = relativeOffset(element._element, x, y);
+		return new HtmlCoordinate(element, relativeCoords.x, relativeCoords.y);
 	};
 
 	HtmlCoordinate.prototype.toRelativeX = function() {
@@ -38,5 +43,31 @@
 		return "[HtmlCoordinate (" + x + ", " + y + ") relative to <" + tag + ">]";
 	};
 
+	function relativeOffset($element, pageX, pageY) {
+		failFastIfStylingPresent($element);
+
+		var pageOffset = $element.offset();
+		return {
+			x: pageX - pageOffset.left,
+			y: pageY - pageOffset.top
+		};
+	}
+
+	function failFastIfStylingPresent($element) {
+		failFastIfPaddingPresent("top");
+		failFastIfPaddingPresent("left");
+		failFastIfBorderPresent("top");
+		failFastIfBorderPresent("left");
+
+		function failFastIfPaddingPresent(side) {
+			var css = $element.css("padding-" + side);
+			if (css !== "0px") throw new Error("Do not apply padding to elements used with relativeOffset() (expected 0px but was " + css + ")");
+		}
+
+		function failFastIfBorderPresent(side) {
+			var css = $element.css("border-" + side + "-width");
+			if (css !== "0px") throw new Error("Do not apply border to elements used with relativeOffset() (expected 0px but was " + css + ")");
+		}
+	}
 
 }());
