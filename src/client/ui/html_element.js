6b8b6a1b941657e428d4174e7668664d16eb39e0
James Shore <jshore@jamesshore.com>
2016-05-05 14:36:05 -0700

    Ripped HtmlCoordinate out of HtmlElement--will put it back later

--- src/client/ui/html_element.js
+++ src/client/ui/html_element.js
@@ -65,9 +65,6 @@
 	HtmlElement.prototype.onMouseLeave = onMouseEventFn("mouseleave");
 	HtmlElement.prototype.onMouseUp = onMouseEventFn("mouseup");
 
-	HtmlElement.prototype.onMouseDown2 = onMouseEventFn2("mousedown");
-	HtmlElement.prototype.onMouseMove2 = onMouseEventFn2("mousemove");
-
 	function triggerMouseEventFn(event) {
 		return function(relativeX, relativeY) {
 			var pageCoords;
@@ -79,16 +76,6 @@
 			}
 
 			sendMouseEvent(this, event, pageCoords);
-		};
-	}
-
-	function onMouseEventFn2(event) {
-		return function(callback) {
-			var self = this;
-			self._element.on(event, function(event) {
-				var coordinate = HtmlCoordinate.fromPageOffset(self, event.pageX, event.pageY);
-				callback(coordinate);
-			});
 		};
 	}
 
@@ -125,9 +112,6 @@
 	HtmlElement.prototype.onSingleTouchStart = onSingleTouchEventFn("touchstart");
 	HtmlElement.prototype.onSingleTouchMove = onSingleTouchEventFn("touchmove");
 	HtmlElement.prototype.onMultiTouchStart = onMultiTouchEventFn("touchstart");
-
-	HtmlElement.prototype.onSingleTouchStart2 = onSingleTouchEventFn2("touchstart");
-	HtmlElement.prototype.onSingleTouchMove2 = onSingleTouchEventFn2("touchmove");
 
 	function triggerZeroTouchEventFn(event) {
 		return function() {
@@ -170,22 +154,6 @@
 				var offset = { x: pageX, y: pageY };
 
 				callback(offset);
-			});
-		};
-	}
-
-	function onSingleTouchEventFn2(eventName) {
-		return function(callback) {
-			var self = this;
-			self._element.on(eventName, function(event) {
-				var originalEvent = event.originalEvent;
-				if (originalEvent.touches.length !== 1) return;
-
-				var pageX = originalEvent.touches[0].pageX;
-				var pageY = originalEvent.touches[0].pageY;
-				var coordinate = HtmlCoordinate.fromPageOffset(self, pageX, pageY);
-
-				callback(coordinate);
 			});
 		};
 	}
