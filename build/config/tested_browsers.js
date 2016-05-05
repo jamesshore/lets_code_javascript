36893262b30bed169969ff555a430643f9070a22
James Shore <jshore@jamesshore.com>
2016-05-05 12:42:09 -0700

    Upgrade Firefox 45 --> 46

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 13.10586.0 (Windows 10 0.0.0)",
-		"Firefox 45.0.0 (Mac OS X 10.11.0)",
+		"Firefox 46.0.0 (Mac OS X 10.11.0)",
 		"Chrome 50.0.2661 (Mac OS X 10.11.4)",
 		"Safari 9.1.0 (Mac OS X 10.11.4)",
 		"Mobile Safari 9.0.0 (iOS 9.3.0)",
