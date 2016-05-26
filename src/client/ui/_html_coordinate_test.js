9f8f6edfea7f12fef3e4589bc992d89eabc603e6
James Shore <jshore@jamesshore.com>
2016-05-26 15:34:56 -0700

    Moved relative offset calculation tests to HtmlCoordinate

--- src/client/ui/_html_coordinate_test.js
+++ src/client/ui/_html_coordinate_test.js
@@ -43,25 +43,25 @@
 			assert.equal(coord.toString(), "[HtmlCoordinate page offset (10, 20)]");
 		});
 
-		describe("coordinate conversion", function() {
+		describe("relative offset conversion", function() {
 
 			var COLLAPSING_BODY_MARGIN = 8;
 
-			it("page coordinate conversion accounts for margin", function() {
-				checkRelativeStyle("margin-top: 13px;", 0, 13 - COLLAPSING_BODY_MARGIN);
-				checkRelativeStyle("margin-left: 13px;", 13, 0);
-				checkRelativeStyle("margin: 13px;", 13, 13 - COLLAPSING_BODY_MARGIN);
-				checkRelativeStyle("margin: 1em; font-size: 16px", 16, 16 - COLLAPSING_BODY_MARGIN);
+			it("'from relative offset' accounts for margin", function() {
+				checkFromRelativeOffsetCalculation("margin-top: 13px;", 0, 13 - COLLAPSING_BODY_MARGIN);
+				checkFromRelativeOffsetCalculation("margin-left: 13px;", 13, 0);
+				checkFromRelativeOffsetCalculation("margin: 13px;", 13, 13 - COLLAPSING_BODY_MARGIN);
+				checkFromRelativeOffsetCalculation("margin: 1em; font-size: 16px", 16, 16 - COLLAPSING_BODY_MARGIN);
 			});
 
-			it("relative coordinate conversion accounts for margin", function() {
-				checkPageStyle("margin-top: 13px;", 0, 13 - COLLAPSING_BODY_MARGIN);
-				checkPageStyle("margin-left: 13px;", 13, 0);
-				checkPageStyle("margin: 13px;", 13, 13 - COLLAPSING_BODY_MARGIN);
-				checkPageStyle("margin: 1em; font-size: 16px", 16, 16 - COLLAPSING_BODY_MARGIN);
+			it("'to relative offset' accounts for margin", function() {
+				checkToRelativeOffsetCalculation("margin-top: 13px;", 0, 13 - COLLAPSING_BODY_MARGIN);
+				checkToRelativeOffsetCalculation("margin-left: 13px;", 13, 0);
+				checkToRelativeOffsetCalculation("margin: 13px;", 13, 13 - COLLAPSING_BODY_MARGIN);
+				checkToRelativeOffsetCalculation("margin: 1em; font-size: 16px", 16, 16 - COLLAPSING_BODY_MARGIN);
 			});
 
-			it("page coordinate conversion fails fast if there is any padding", function() {
+			it("fails fails fast if there is any padding", function() {
 				expectFailFast("padding-top: 13px;");
 				expectFailFast("padding-left: 13px;");
 				expectFailFast("padding: 13px;");
@@ -72,7 +72,7 @@
 				expectFailFast("padding-left: 20%");
 			});
 
-			it("page coordinate conversion fails fast if there is any border", function() {
+			it("fails fast if there is any border", function() {
 				expectFailFast("border-top: 13px solid;");
 				expectFailFast("border-left: 13px solid;");
 				expectFailFast("border: 13px solid;");
@@ -82,8 +82,8 @@
 				expectFailFast("border: thin solid");
 				expectFailFast("border: medium solid");
 				expectFailFast("border: thick solid");
-				checkRelativeStyle("border: 13px none", 0, 0);
-				checkPageStyle("border: 13px none", 0, 0);
+				checkFromRelativeOffsetCalculation("border: 13px none", 0, 0);
+				checkToRelativeOffsetCalculation("border: 13px none", 0, 0);
 			});
 
 			function expectFailFast(elementStyle) {
@@ -91,10 +91,10 @@
 				try {
 					styledElement.appendSelfToBody();
 					assert.throws(function() {
-						styledElement.relativeOffset({ x: 100, y: 150 });
+						HtmlCoordinate.fromRelativeOffset(styledElement,  100, 150);
 					});
 					assert.throws(function() {
-						styledElement.pageOffset({ x: 100, y: 150 });
+						HtmlCoordinate.fromPageOffset(100, 150).toRelativeOffset(styledElement);
 					});
 				}
 				finally {
@@ -102,67 +102,49 @@
 				}
 			}
 
-			function checkRelativeStyle(elementStyle, additionalXOffset, additionalYOffset) {
+			function checkFromRelativeOffsetCalculation(elementStyle, additionalXOffset, additionalYOffset) {
 				var BASE_STYLE = "width: 120px; height: 80px; border: 0px none;";
 
 				var unstyledElement = HtmlElement.fromHtml("<div style='" + BASE_STYLE + "'></div>");
 				unstyledElement.appendSelfToBody();
-				var unstyledOffset = unstyledElement.relativeOffset({x: 100, y: 150});
-
-				var unstyledCoord = HtmlCoordinate.fromRelativeOffset(unstyledElement, 100, 150);
-
+				var expectedCoord = HtmlCoordinate.fromRelativeOffset(
+					unstyledElement,
+					100 + additionalXOffset,
+					150 + additionalYOffset
+				);
 				unstyledElement.remove();
 
 				var styledElement = HtmlElement.fromHtml("<div style='" + BASE_STYLE + elementStyle + "'></div>");
 				try {
 					styledElement.appendSelfToBody();
-
-					var styledCoord = HtmlCoordinate.fromRelativeOffset(styledElement, 100, 150);
-
-					var styledOffset = styledElement.relativeOffset({x: 100, y: 150});
-
-					assert.objEqual(unstyledCoord, styledCoord);
-
-					assertRelativeOffsetEquals(
-						styledOffset,
-						unstyledOffset.x - additionalXOffset,
-						unstyledOffset.y - additionalYOffset
-					);
+					var actualCoord = HtmlCoordinate.fromRelativeOffset(styledElement, 100, 150);
+					assert.objEqual(expectedCoord, actualCoord);
 				}
 				finally {
 					styledElement.remove();
 				}
 			}
 
-			function checkPageStyle(elementStyle, additionalXOffset, additionalYOffset) {
+			function checkToRelativeOffsetCalculation(elementStyle, additionalXOffset, additionalYOffset) {
 				var BASE_STYLE = "width: 120px; height: 80px; border: 0px none;";
 
 				var unstyledElement = HtmlElement.fromHtml("<div style='" + BASE_STYLE + "'></div>");
 				unstyledElement.appendSelfToBody();
-				var unstyledOffset = unstyledElement.pageOffset({x: 100, y: 150});
+				var expectedOffset = HtmlCoordinate.fromPageOffset(
+					100 - additionalXOffset,
+					150 - additionalYOffset
+				).toRelativeOffset(unstyledElement);
 				unstyledElement.remove();
 
 				var styledElement = HtmlElement.fromHtml("<div style='" + BASE_STYLE + elementStyle + "'></div>");
 				try {
 					styledElement.appendSelfToBody();
-					var styledOffset = styledElement.pageOffset({x: 100, y: 150});
-					assertRelativeOffsetEquals(
-						styledOffset,
-						unstyledOffset.x + additionalXOffset,
-						unstyledOffset.y + additionalYOffset
-					);
+					var actualOffset = HtmlCoordinate.fromPageOffset(100, 150).toRelativeOffset(styledElement);
+					assert.deepEqual(expectedOffset, actualOffset);
 				}
 				finally {
 					styledElement.remove();
 				}
-			}
-
-			function assertRelativeOffsetEquals(actualOffset, expectedX, expectedY) {
-				assert.deepEqual(actualOffset, {x: expectedX, y: expectedY});
-			}
-
-			function assertPageOffsetEquals(actualOffset, expectedX, expectedY) {
-				assert.deepEqual(actualOffset, {x: expectedX, y: expectedY});
 			}
 
 		});
