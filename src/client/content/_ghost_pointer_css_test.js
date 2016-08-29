bd66189867204e40f5ff5837f248283ccd06ef22
James Shore <jshore@jamesshore.com>
2016-08-29 12:50:15 -0700

    Added 'ghost-pointer' CSS block and updated index.html

---
+++ src/client/content/_ghost_pointer_css_test.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2016 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var assert = require("../../shared/_assert.js");
+	var cssHelper = require("./_css_test_helper.js");
+
+	describe("CSS: Ghost pointer block", function() {
+
+		cssHelper.setupUnitTests();
+
+		var pointer;
+
+		beforeEach(function() {
+			pointer = cssHelper.frame.add("<div class='ghost-pointer'>img</div>", "pointer");
+		});
+
+		it("is slightly transparent", function() {
+			assert.equal(cssHelper.opacity(pointer), "0.5", "opacity");
+		});
+
+	});
+
+}());
