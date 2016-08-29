b2a40e5fd6a7c4d8917e688d842ddda37a92e298
James Shore <jshore@jamesshore.com>
2016-08-29 10:00:16 -0700

    Upgraded Firefox 47 --> 48

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 13.10586.0 (Windows 10 0.0.0)",
-		"Firefox 47.0.0 (Mac OS X 10.11.0)",
+		"Firefox 48.0.0 (Mac OS X 10.11.0)",
 		"Chrome 51.0.2704 (Mac OS X 10.11.5)",
 		"Safari 9.1.1 (Mac OS X 10.11.5)",
 		"Mobile Safari 9.0.0 (iOS 9.3.0)",
