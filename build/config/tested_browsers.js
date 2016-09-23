3b575f7f0d60fe012cc94c7e570087ad73a40105
James Shore <jshore@jamesshore.com>
2016-09-23 13:56:40 -0700

    Upgraded Chrome 52 --> 53

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 13.10586.0 (Windows 10 0.0.0)",
 		"Firefox 48.0.0 (Mac OS X 10.11.0)",
-		"Chrome 52.0.2743 (Mac OS X 10.11.6)",
+		"Chrome 53.0.2785 (Mac OS X 10.11.6)",
 		"Safari 9.1.3 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.0.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
