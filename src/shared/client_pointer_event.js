3e0f5790b71470a6b168909100638788ef16748a
James Shore <jshore@jamesshore.com>
2016-09-30 14:36:49 -0700

    Fixed incorrect function name in ClientPointerEvent

--- src/shared/client_pointer_event.js
+++ src/shared/client_pointer_event.js
@@ -4,25 +4,25 @@
 
 	var ServerPointerEvent = require("./server_pointer_event.js");
 
-	var ClientPointerEvent = module.exports = function ServerPointerEvent(x, y) {
+	var ClientPointerEvent = module.exports = function(x, y) {
 		this._x = x;
 		this._y = y;
 	};
 
 	ClientPointerEvent.EVENT_NAME = "client_pointer_event";
 
-	ClientPointerEvent.fromSerializableObject = function fromSerializableObject(obj) {
+	ClientPointerEvent.fromSerializableObject = function(obj) {
 		return new ClientPointerEvent(obj.x, obj.y);
 	};
 
-	ClientPointerEvent.prototype.toSerializableObject = function toSerializableObject() {
+	ClientPointerEvent.prototype.toSerializableObject = function() {
 		return {
 			x: this._x,
 			y: this._y
 		};
 	};
 
-	ClientPointerEvent.prototype.toServerEvent = function toServerEvent(id) {
+	ClientPointerEvent.prototype.toServerEvent = function(id) {
 		return new ServerPointerEvent(id, this._x, this._y);
 	};
 
