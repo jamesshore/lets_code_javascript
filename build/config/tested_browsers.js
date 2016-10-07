f1d05e969155a42b6008c0ee3f96938ab5f5874f
James Shore <jshore@jamesshore.com>
2016-10-07 14:53:45 -0700

    Upgrade MacOS Safari 9.1.3 --> 10.0.0

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -7,7 +7,7 @@
 		"Edge 13.10586.0 (Windows 10 0.0.0)",
 		"Firefox 49.0.0 (Mac OS X 10.11.0)",
 		"Chrome 53.0.2785 (Mac OS X 10.11.6)",
-		"Safari 9.1.3 (Mac OS X 10.11.6)",
+		"Safari 10.0.0 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.0.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 	];
