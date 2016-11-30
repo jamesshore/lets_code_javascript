9344eaab4e21242ca7960aa6f92690e630476268
James Shore <jshore@jamesshore.com>
2016-11-30 14:08:19 -0800

    Upgrade Firefox 49 --> 50

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
-		"Firefox 49.0.0 (Mac OS X 10.11.0)",
+		"Firefox 50.0.0 (Mac OS X 10.11.0)",
 		"Chrome 54.0.2840 (Mac OS X 10.11.6)",
 		"Safari 10.0.1 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.1.0)",
