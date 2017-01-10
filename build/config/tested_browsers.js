0947061707b374aa4e6e428b480f6fc2e9f5709a
James Shore <jshore@jamesshore.com>
2017-01-10 11:33:07 -0800

    Upgrade Safari 10.0.1 --> 10.0.2

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -7,7 +7,7 @@
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
 		"Firefox 50.0.0 (Mac OS X 10.11.0)",
 		"Chrome 55.0.2883 (Mac OS X 10.11.6)",
-		"Safari 10.0.1 (Mac OS X 10.11.6)",
+		"Safari 10.0.2 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.1.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 	];
