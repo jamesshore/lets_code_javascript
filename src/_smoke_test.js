0d62619e69c8fd16b60c3e82a3b1cf1035bb603a
James Shore <jshore@jamesshore.com>
2017-02-02 12:35:59 -0800

    Upgrade smoke test firefox 51.0 --> 51.0.1

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -19,7 +19,7 @@
 
 	var HOME_PAGE_URL = "http://localhost:5000";
 	var NOT_FOUND_PAGE_URL = "http://localhost:5000/xxx";
-	var EXPECTED_BROWSER = "firefox 51.0";
+	var EXPECTED_BROWSER = "firefox 51.0.1";
 	var GHOST_POINTER_SELECTOR = ".ghost-pointer";
 	var DRAWING_AREA_ID = "drawing-area";
 
