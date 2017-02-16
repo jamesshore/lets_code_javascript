d0e30219401fc5ed5c9f43c3ee48f072768f94d5
James Shore <jshore@jamesshore.com>
2017-02-16 10:52:25 -0800

    Upgrade Chrome 55 --> 56

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
 		"Firefox 51.0.0 (Mac OS X 10.11.0)",
-		"Chrome 55.0.2883 (Mac OS X 10.11.6)",
+		"Chrome 56.0.2924 (Mac OS X 10.11.6)",
 		"Safari 10.0.3 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
