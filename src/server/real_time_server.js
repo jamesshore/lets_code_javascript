230d635d911335cca78f085c3a734bfc2d1c49f5
James Shore <jshore@jamesshore.com>
2017-04-13 14:53:02 -0700

    Start factoring out RealTimeServer from server.js

---
+++ src/server/real_time_server.js
@@ -0,0 +1,68 @@
+// Copyright (c) 2017 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var io = require('socket.io');
+	var ClientPointerEvent = require("../shared/client_pointer_event.js");
+	var ClientRemovePointerEvent = require("../shared/client_remove_pointer_event.js");
+	var ClientDrawEvent = require("../shared/client_draw_event.js");
+	var ClientClearScreenEvent = require("../shared/client_clear_screen_event.js");
+	var EventRepository = require("./event_repository.js");
+
+	var RealTimeServer = module.exports = function RealTimeServer() {};
+
+	RealTimeServer.prototype.start = function() {
+		// this._ioServer = io(this._httpServer);
+		// RealTimeServer.handleSocketIoEvents(this, this._ioServer);
+	};
+
+	RealTimeServer.handleSocketIoEvents = function handleSocketIoEvents(self, ioServer) {
+		self._eventRepo = new EventRepository();
+
+		ioServer.on("connect", function(socket) {
+			replayPreviousEvents(self, socket);
+			reflectClientEventsWithId(self, socket);
+			reflectClientEventsWithoutId(self, socket);
+		});
+	};
+
+	function replayPreviousEvents(self, socket) {
+		self._eventRepo.replay().forEach(function(event) {
+			socket.emit(event.name(), event.toSerializableObject());
+		});
+	}
+
+	function reflectClientEventsWithId(self, socket) {
+		var supportedEvents = [
+			ClientPointerEvent,
+			ClientRemovePointerEvent
+		];
+
+		supportedEvents.forEach(function(eventConstructor) {
+			socket.on(eventConstructor.EVENT_NAME, function(eventData) {
+				var clientEvent = eventConstructor.fromSerializableObject(eventData);
+				var serverEvent = clientEvent.toServerEvent(socket.id);
+				self._eventRepo.store(serverEvent);
+				socket.broadcast.emit(serverEvent.name(), serverEvent.toSerializableObject());
+			});
+		});
+	}
+
+	function reflectClientEventsWithoutId(self, socket) {
+		var supportedEvents = [
+			ClientDrawEvent,
+			ClientClearScreenEvent
+		];
+
+		supportedEvents.forEach(function(eventConstructor) {
+			socket.on(eventConstructor.EVENT_NAME, function(eventData) {
+				var clientEvent = eventConstructor.fromSerializableObject(eventData);
+				var serverEvent = clientEvent.toServerEvent();
+				self._eventRepo.store(serverEvent);
+				socket.broadcast.emit(serverEvent.name(), serverEvent.toSerializableObject());
+			});
+		});
+	}
+
+
+}());
--- src/server/server.js
+++ src/server/server.js
@@ -6,11 +6,7 @@
 	var fs = require("fs");
 	var send = require("send");
 	var io = require('socket.io');
-	var ClientPointerEvent = require("../shared/client_pointer_event.js");
-	var ClientRemovePointerEvent = require("../shared/client_remove_pointer_event.js");
-	var ClientDrawEvent = require("../shared/client_draw_event.js");
-	var ClientClearScreenEvent = require("../shared/client_clear_screen_event.js");
-	var EventRepository = require("./event_repository.js");
+	var RealTimeServer = require("./real_time_server.js");
 
 	var Server = module.exports = function Server() {};
 
@@ -20,8 +16,11 @@
 		this._httpServer = http.createServer();
 		handleHttpRequests(this._httpServer, contentDir, notFoundPageToServe);
 
+		var realTimeServer = new RealTimeServer();
+		realTimeServer.start();
+
 		this._ioServer = io(this._httpServer);
-		handleSocketIoEvents(this, this._ioServer);
+		RealTimeServer.handleSocketIoEvents(this, this._ioServer);
 
 		this._httpServer.listen(portNumber, callback);
 	};
@@ -40,54 +39,6 @@
 				if (err.status === 404) serveErrorFile(response, 404, contentDir + "/" + notFoundPageToServe);
 				else throw err;
 			}
-		});
-	}
-
-	function handleSocketIoEvents(self, ioServer) {
-		self._eventRepo = new EventRepository();
-
-		ioServer.on("connect", function(socket) {
-			replayPreviousEvents(self, socket);
-			reflectClientEventsWithId(self, socket);
-			reflectClientEventsWithoutId(self, socket);
-		});
-	}
-
-	function replayPreviousEvents(self, socket) {
-		self._eventRepo.replay().forEach(function(event) {
-			socket.emit(event.name(), event.toSerializableObject());
-		});
-	}
-
-	function reflectClientEventsWithId(self, socket) {
-		var supportedEvents = [
-			ClientPointerEvent,
-			ClientRemovePointerEvent
-		];
-
-		supportedEvents.forEach(function(eventConstructor) {
-			socket.on(eventConstructor.EVENT_NAME, function(eventData) {
-				var clientEvent = eventConstructor.fromSerializableObject(eventData);
-				var serverEvent = clientEvent.toServerEvent(socket.id);
-				self._eventRepo.store(serverEvent);
-				socket.broadcast.emit(serverEvent.name(), serverEvent.toSerializableObject());
-			});
-		});
-	}
-
-	function reflectClientEventsWithoutId(self, socket) {
-		var supportedEvents = [
-			ClientDrawEvent,
-			ClientClearScreenEvent
-		];
-
-		supportedEvents.forEach(function(eventConstructor) {
-			socket.on(eventConstructor.EVENT_NAME, function(eventData) {
-				var clientEvent = eventConstructor.fromSerializableObject(eventData);
-				var serverEvent = clientEvent.toServerEvent();
-				self._eventRepo.store(serverEvent);
-				socket.broadcast.emit(serverEvent.name(), serverEvent.toSerializableObject());
-			});
 		});
 	}
 
