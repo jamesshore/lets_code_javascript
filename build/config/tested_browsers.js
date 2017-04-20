9a587581860c5fb5053b4877d10f6bc043aa3b50
James Shore <jshore@jamesshore.com>
2017-04-20 12:21:31 -0700

    Upgrade Firefox 52 --> 53

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
-		"Firefox 52.0.0 (Mac OS X 10.11.0)",
+		"Firefox 53.0.0 (Mac OS X 10.11.0)",
 		"Chrome 57.0.2987 (Mac OS X 10.11.6)",
 		"Safari 10.1.0 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
