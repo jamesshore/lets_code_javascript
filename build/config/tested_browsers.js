95bba522d658a99f68fff6f72dc38d446e965cdd
James Shore <jshore@jamesshore.com>
2017-07-14 12:20:53 -0700

    Upgrade Chrome 58 --> 59

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
 		"Firefox 54.0.0 (Mac OS X 10.11.0)",
-		"Chrome 58.0.3029 (Mac OS X 10.11.6)",
+		"Chrome 59.0.3071 (Mac OS X 10.11.6)",
 		"Safari 10.1.1 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
 		"Chrome Mobile 44.0.2403 (Android 6.0.0)"
