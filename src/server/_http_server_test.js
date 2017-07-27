e1417b9d6ed46eacafd290f044b8aede0f83ebcd
James Shore <jshore@jamesshore.com>
2017-07-27 12:31:26 -0700

    Convert HttpServerTest's beforeEach/afterEach to use async/await

--- src/server/_http_server_test.js
+++ src/server/_http_server_test.js
@@ -2,28 +2,30 @@
 (function() {
 	"use strict";
 
-	var HttpServer = require("./http_server.js");
-	var assert = require("_assert");
-	var async = require("async");
-	var http = require("http");
-	var fs = require("fs");
+	const HttpServer = require("./http_server.js");
+	const assert = require("_assert");
+	const async = require("async");
+	const http = require("http");
+	const fs = require("fs");
+	const util = require("util");
 
-	var CONTENT_DIR = "generated/test";
-	var IRRELEVANT_DIR = "irrelevant";
+	const writeFile = util.promisify(fs.writeFile);
+	const unlink = util.promisify(fs.unlink);
 
-	var INDEX_PAGE = "index.html";
-	var OTHER_PAGE = "other.html";
-	var NOT_FOUND_PAGE = "test404.html";
-	var IRRELEVANT_PAGE = "irrelevant";
+	const CONTENT_DIR = "generated/test";
 
-	var INDEX_PAGE_DATA = "This is index page file";
-	var OTHER_PAGE_DATA = "This is another page";
-	var NOT_FOUND_DATA = "This is 404 page file";
+	const INDEX_PAGE = "index.html";
+	const OTHER_PAGE = "other.html";
+	const NOT_FOUND_PAGE = "test404.html";
 
-	var PORT = 5020;
-	var BASE_URL = "http://localhost:" + PORT;
+	const INDEX_PAGE_DATA = "This is index page file";
+	const OTHER_PAGE_DATA = "This is another page";
+	const NOT_FOUND_DATA = "This is 404 page file";
 
-	var TEST_FILES = [
+	const PORT = 5020;
+	const BASE_URL = "http://localhost:" + PORT;
+
+	const TEST_FILES = [
 		[ CONTENT_DIR + "/" + INDEX_PAGE, INDEX_PAGE_DATA],
 		[ CONTENT_DIR + "/" + OTHER_PAGE, OTHER_PAGE_DATA],
 		[ CONTENT_DIR + "/" + NOT_FOUND_PAGE, NOT_FOUND_DATA]
@@ -31,19 +33,20 @@
 
 	describe("HTTP Server", function() {
 
-		let server;
+		let server = new HttpServer(CONTENT_DIR, NOT_FOUND_PAGE);
 
-		beforeEach(function(done) {
-			async.each(TEST_FILES, createTestFile, function() {
-				server = new HttpServer(CONTENT_DIR, NOT_FOUND_PAGE);
-				server.start(PORT).then(done);
-			});
+		beforeEach(async () => {
+			await Promise.all([
+				createTestFiles(),
+				server.start(PORT)
+			]);
 		});
 
-		afterEach(function(done) {
-			async.each(TEST_FILES, deleteTestFile, function() {
-				server.stop().then(done);
-			});
+		afterEach(async () => {
+			await Promise.all([
+				deleteTestFiles(),
+				server.stop()
+			]);
 		});
 
 		it("serves files from directory", async () => {
@@ -83,7 +86,7 @@
 		function httpGet(url) {
 			return new Promise((resolve, reject) => {
 				http.get(url, function(response) {
-					var receivedData = "";
+					let receivedData = "";
 					response.setEncoding("utf8");
 
 					response.on("data", function(chunk) {
@@ -99,30 +102,16 @@
 
 	});
 
-	function createTestFile(fileAndData, done) {
-		// Note: writeFile() MUST be called asynchronously in order for this code to work on Windows 7.
-		// If it's called synchronously, it fails with an EPERM error when the second test starts. This
-		// may be related to this Node.js issue: https://github.com/joyent/node/issues/6599
-		// This issue appeared after upgrading send 0.2.0 to 0.9.3. Prior to that, writeFileSync()
-		// worked fine.
-		fs.writeFile(fileAndData[0], fileAndData[1], function(err) {
-			if (err) console.log("could not create test file: [" + fileAndData[0] + "]. Error: " + err);
-			done();
-		});
+	function createTestFiles() {
+		return Promise.all(TEST_FILES.map(([ file, data ]) => {
+			return writeFile(file, data);
+		}));
 	}
 
-	function deleteTestFile(fileAndData, done) {
-		// Note: unlink() MUST be called asynchronously here in order for this code to work on Windows 7.
-		// If it's called synchronously, then it will run before the file is closed, and that is not allowed
-		// on Windows 7. It's possible that this is the result of a Node.js bug; see this issue for details:
-		// https://github.com/joyent/node/issues/6599
-		var file = fileAndData[0];
-		if (fs.existsSync(file)) {
-			fs.unlink(file, function(err) {
-				if (err || fs.existsSync(file)) console.log("could not delete test file: [" + file + "]. Error: " + err);
-				done();
-			});
-		}
+	function deleteTestFiles() {
+		return Promise.all(TEST_FILES.map(([ file ]) => {
+			return unlink(file);
+		}));
 	}
 
 }());
