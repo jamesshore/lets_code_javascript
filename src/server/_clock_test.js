57e8803fa714e51386170bb8b4578f88e4e4a173
James Shore <jshore@jamesshore.com>
2017-08-11 14:28:12 -0700

    Create Clock class

---
+++ src/server/_clock_test.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	const assert = require("_assert");
+	const Clock = require("./clock.js");
+
+	describe("Clock", function() {
+
+		it("instantiates", function() {
+			const clock = new Clock();
+		});
+
+	});
+
+}());
---
+++ src/server/clock.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	module.exports = class Clock {
+
+	};
+
+}());
