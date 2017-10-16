b0b0f261b9cf8c7f2d3d4f9ef1270d8066b1fe28
James Shore <jshore@jamesshore.com>
2017-10-16 12:15:02 -0700

    Upgrade Firefox 54 --> 56

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,7 +5,7 @@
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
-		"Firefox 54.0.0 (Mac OS X 10.11.0)",
+		"Firefox 56.0.0 (Mac OS X 10.11.0)",
 		"Chrome 60.0.3112 (Mac OS X 10.11.6)",
 		"Safari 10.1.2 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
