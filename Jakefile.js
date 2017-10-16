50b10de0a5c056957c284f83837e56d8cb6157f2
James Shore <jshore@jamesshore.com>
2017-10-16 12:14:03 -0700

    Check dependency versions in 'jake run'

--- Jakefile.js
+++ Jakefile.js
@@ -51,7 +51,7 @@
 	}, { async: true });
 
 	desc("Start localhost server for manual testing");
-	task("run", [ "build" ], function() {
+	task("run", [ "versions", "build" ], function() {
 		var runServer = require("./src/_run_server.js");
 
 		console.log("Running server. Press Ctrl-C to stop.");
