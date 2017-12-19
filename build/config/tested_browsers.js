4e5db0c0633cc285d5e6cb890834330eee3faea8
James Shore <jshore@jamesshore.com>
2017-12-19 10:32:18 -0800

    Upgrade Safari 11.0.1 --> 11.0.2

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -7,7 +7,7 @@
 		"Edge 14.14393.0 (Windows 10 0.0.0)",
 		"Firefox 57.0.0 (Mac OS X 10.11.0)",
 		"Chrome 63.0.3239 (Mac OS X 10.11.6)",
-		"Safari 11.0.1 (Mac OS X 10.11.6)",
+		"Safari 11.0.2 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
 		"Chrome Mobile WebView 44.0.2403 (Android 6.0.0)"
 	];
