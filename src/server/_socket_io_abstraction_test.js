18a7fa3ee9c9b23f7ce8faf027b5bf86549f6276
James Shore <jshore@jamesshore.com>
2017-12-19 13:26:38 -0800

    Move tests from RealTimeServer to SocketIoAbstraction; not done yet

--- src/server/_socket_io_abstraction_test.js
+++ src/server/_socket_io_abstraction_test.js
@@ -6,6 +6,8 @@
 	const SocketIoClient = require("./__socket_io_client.js");
 	const HttpServer = require("./http_server.js");
 	const RealTimeServer = require("./real_time_server.js");
+	const ClientRemovePointerEvent = require("../shared/client_remove_pointer_event.js");
+	const ClientPointerEvent = require("../shared/client_pointer_event.js");
 
 	describe("Socket.IO Abstraction", function() {
 
@@ -15,11 +17,13 @@
 
 		let httpServer;
 		let realTimeServer;
+		let socketIoAbstraction;
 		let socketIoClient;
 
 		beforeEach(async function() {
 			httpServer = new HttpServer(IRRELEVANT_DIR, IRRELEVANT_PAGE);
 			realTimeServer = new RealTimeServer();
+			socketIoAbstraction = realTimeServer._socketIoAbstraction;
 			socketIoClient = new SocketIoClient("http://localhost:" + PORT, realTimeServer._socketIoAbstraction);
 
 			realTimeServer.start(httpServer.getNodeServer());
@@ -48,6 +52,69 @@
 				done();
 			});
 			// if the bug occurs, the afterEach() function will time out
+		});
+
+		it("counts the number of connections", async function() {
+			assert.equal(realTimeServer.numberOfActiveConnections(), 0, "before opening connection");
+
+			const socket = await socketIoClient.createSocket();
+			assert.equal(realTimeServer.numberOfActiveConnections(), 1, "after opening connection");
+
+			await socketIoClient.closeSocket(socket);
+		});
+
+		it("tells us if a socket is connected", async function() {
+			assert.equal(realTimeServer._socketIoAbstraction.isClientConnected("no_such_socket"), false);
+
+			const socket = await socketIoClient.createSocket();
+			assert.equal(realTimeServer._socketIoAbstraction.isClientConnected(socket.id), true);
+
+			await socketIoClient.closeSocket(socket);
+		});
+
+		it("emits an event when a Socket.IO client event is received", async function() {
+			const socket = await socketIoClient.createSocket();
+			const sentEvent = new ClientRemovePointerEvent();
+
+			const serverPromise = new Promise((resolve, reject) => {
+				realTimeServer.onNextClientEvent((socketId, receivedEvent) => {
+					try {
+						assert.equal(socketId, socket.id, "socket ID");
+						assert.deepEqual(receivedEvent, sentEvent, "event");
+						resolve();
+					}
+					catch (err) {
+						reject(err);
+					}
+				});
+			});
+			socket.emit(sentEvent.name(), sentEvent.payload());
+
+			await serverPromise;
+			await socketIoClient.closeSocket(socket);
+		});
+
+		it("emits an event when the server has emitted a Socket.IO event", async function() {
+			const emitter = await socketIoClient.createSocket();
+
+			const clientEvent = new ClientPointerEvent(10, 20);
+			const emitPromise = new Promise((resolve, reject) => {
+				realTimeServer.onNextServerEmit((event) => {
+					try {
+						const serverEvent = clientEvent.toServerEvent(emitter.id);
+						assert.deepEqual(event, serverEvent, "event");
+						resolve();
+					}
+					catch (err) {
+						reject(err);
+					}
+				});
+			});
+
+			emitter.emit(clientEvent.name(), clientEvent.payload());
+
+			await emitPromise;
+			await socketIoClient.closeSocket(emitter);
 		});
 
 	});
