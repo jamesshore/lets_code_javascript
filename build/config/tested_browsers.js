82495cfb9a0c799f513f736c783ace40118087ca
James Shore <jshore@jamesshore.com>
2018-03-02 16:38:59 -0800

    Upgrade Edge 14 --> 16

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -4,7 +4,7 @@
 
 	module.exports = [
 		"IE 11.0.0 (Windows 7 0.0.0)",
-		"Edge 14.14393.0 (Windows 10 0.0.0)",
+		"Edge 16.16299.0 (Windows 10 0.0.0)",
 		"Firefox 58.0.0 (Mac OS X 10.11.0)",
 		"Chrome 64.0.3282 (Mac OS X 10.11.6)",
 		"Safari 11.0.3 (Mac OS X 10.11.6)",
