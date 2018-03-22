863d3a789244744d040b3996d6f78c222d9290c9
James Shore <jshore@jamesshore.com>
2018-03-22 10:46:56 -0700

    Upgrade Chrome 64 --> 65

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -6,7 +6,7 @@
 		"IE 11.0.0 (Windows 7.0.0)",
 		"Edge 16.16299.0 (Windows 10.0.0)",
 		"Firefox 59.0.0 (Mac OS X 10.11.0)",
-		"Chrome 64.0.3282 (Mac OS X 10.11.6)",
+		"Chrome 65.0.3325 (Mac OS X 10.11.6)",
 		"Safari 11.0.3 (Mac OS X 10.11.6)",
 		"Mobile Safari 10.0.0 (iOS 10.2.0)",
 		"Chrome Mobile WebView 44.0.2403 (Android 6.0.0)"
